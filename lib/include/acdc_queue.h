42c3409623e058372029f83e205fa860550a8f14
Jeff Fifield <jeff.fifield@xilinx.com>
2020-11-11 07:50:00 -0700

    Support signals in microblaze queue

--- lib/include/acdc_queue.h
+++ lib/include/acdc_queue.h
@@ -1,32 +1,131 @@
 #include <stdint.h>
+#include "hsa_defs.h"
+
+#define MB_QUEUE_SIZE 64
 
 typedef struct dispatch_packet_s {
-    
-    // HSA-like interface
-    uint16_t header;
-    uint16_t type;
-    uint32_t reserved0;
-    uint64_t return_address;
-    uint64_t arg[4];
-    uint64_t reserved1;
-    uint64_t completion_signal;
+  
+  // HSA-like interface
+  uint16_t header;
+  uint16_t type;
+  uint32_t reserved0;
+  uint64_t return_address;
+  uint64_t arg[4];
+  uint64_t reserved1;
+  uint64_t completion_signal;
 
 } __attribute__((packed)) dispatch_packet_t;
 
 typedef struct queue_s {
 
-    // HSA-like interface
-    uint32_t type;
-    uint32_t features;
-    uint64_t base_address;
-    volatile uint64_t doorbell;
-    uint32_t size;
-    uint32_t reserved0;
-    uint64_t id;
+  // HSA-like interface
+  uint32_t type;
+  uint32_t features;
+  uint64_t base_address;
+  uint64_t doorbell;
+  uint32_t size;
+  uint32_t reserved0;
+  uint64_t id;
 
-    // implementation detail
-    uint64_t read_index;
-    uint64_t write_index;
-    uint64_t last_doorbell;
+  // implementation detail
+  uint64_t read_index;
+  uint64_t write_index;
+  uint64_t last_doorbell;
+
+  uint64_t base_address_paddr;
+  uint64_t base_address_vaddr;
 
 } __attribute__((packed)) queue_t;
+
+typedef struct signal_s {
+  uint64_t handle;
+} signal_t;
+
+typedef uint64_t signal_value_t;
+
+namespace {
+
+inline uint64_t queue_add_write_index(queue_t *q, uint64_t v)
+{
+  auto r = q->write_index;
+  q->write_index = r + v;
+  return r;
+}
+
+inline uint64_t queue_add_read_index(queue_t *q, uint64_t v)
+{
+  auto r = q->read_index;
+  q->read_index = r + v;
+  return r;
+}
+
+inline uint64_t queue_load_read_index(queue_t *q)
+{
+  return q->read_index;
+}
+
+inline uint64_t queue_load_write_index(queue_t *q)
+{
+  return q->write_index;
+}
+
+inline bool packet_get_active(dispatch_packet_t *pkt)
+{
+  return pkt->reserved1 & 0x1;
+}
+
+inline void packet_set_active(dispatch_packet_t *pkt, bool b)
+{
+  pkt->reserved1 = (pkt->reserved1 & ~0x1) | b;
+}
+
+inline void initialize_packet(dispatch_packet_t *pkt)
+{
+  pkt->header = 0;
+  pkt->type = HSA_PACKET_TYPE_INVALID;
+}
+
+inline hsa_status_t signal_create(signal_value_t initial_value, uint32_t num_consumers, void *consumers, signal_t *signal)
+{
+  //auto s = (signal_value_t*)malloc(sizeof(signal_value_t));
+  //*s = initial_value;
+  signal->handle = (uint64_t)initial_value;
+  return HSA_STATUS_SUCCESS;
+}
+
+inline hsa_status_t signal_destroy(signal_t signal)
+{
+  //free((void*)signal.handle);
+}
+
+inline void signal_store_release(signal_t *signal, signal_value_t value)
+{
+  signal->handle = (uint64_t)value;
+}
+
+inline signal_value_t signal_wait_aquire(volatile signal_t *signal,
+                                         hsa_signal_condition_t condition,
+                                         signal_value_t compare_value,
+                                         uint64_t timeout_hint,
+                                         hsa_wait_state_t wait_state_hint)
+{
+  signal_value_t ret = 0;
+  uint64_t timeout = timeout_hint;
+  do {
+    ret = signal->handle;
+    if (ret == compare_value)
+      return compare_value;
+  } while (timeout--);
+  return ret;
+}
+
+inline void signal_subtract_acq_rel(signal_t *signal, signal_value_t value)
+{
+  signal->handle = signal->handle - (uint64_t)value;
+  // uint64_t i;
+  // memcpy((void*)&i, (void*)signal, sizeof(signal_value_t));
+  // i = i - (uint64_t)value;
+  // memcpy((void*)signal, (void*)&i, sizeof(signal_value_t));
+}
+
+}
--- lib/mb/main.cpp
+++ lib/mb/main.cpp
@@ -4,6 +4,8 @@
 
 extern "C" {
 #include "xil_printf.h"
+
+#include "xaiengine.h"
 #include "mb_interface.h"
 
 #include "acdc_queue.h"
@@ -12,7 +14,52 @@ extern "C" {
 
 #include "platform.h"
 
-#define MB_QUEUE_SIZE 128
+#define XAIE_NUM_ROWS            8
+#define XAIE_NUM_COLS           50
+#define XAIE_ADDR_ARRAY_OFF     0x800
+
+#define HIGH_ADDR(addr)	((addr & 0xffffffff00000000) >> 32)
+#define LOW_ADDR(addr)	(addr & 0x00000000ffffffff)
+
+namespace {
+
+XAieGbl_Config *AieConfigPtr;	                          /**< AIE configuration pointer */
+XAieGbl AieInst;	                                      /**< AIE global instance */
+XAieGbl_HwCfg AieConfig;                                /**< AIE HW configuration instance */
+XAieGbl_Tile TileInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];  /**< Instantiates AIE array of [XAIE_NUM_COLS] x [XAIE_NUM_ROWS] */
+XAieDma_Tile TileDMAInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];
+
+int xaie_lock_release(u32 col, u32 row, u32 lock_id, u32 val)
+{
+  //XAieTile_LockRelease(&(TileInst[col][row]), lock_id, val, 0);
+  return 1;
+}
+
+int xaie_lock_acquire_nb(u32 col, u32 row, u32 lock_id, u32 val)
+{
+  u8 lock_ret = 0;
+  u32 loop = 0;
+  while ((!lock_ret) && (loop < 512)) {
+    //lock_ret = XAieTile_LockAcquire(&(TileInst[col][row]), lock_id, val, 10000);
+    loop++;
+  }
+  if (loop == 512) {
+    //xil_printf("Acquire of lock [%d, %d, %d] with value %d timed out\n", col, row, lock_id, val);
+    return 0;
+  }
+  return 1;
+}
+
+void xaie_init()
+{
+  size_t aie_base = XAIE_ADDR_ARRAY_OFF << 14;
+  // XAIEGBL_HWCFG_SET_CONFIG((&AieConfig), XAIE_NUM_ROWS, XAIE_NUM_COLS, XAIE_ADDR_ARRAY_OFF);
+  // XAieGbl_HwInit(&AieConfig);
+  // AieConfigPtr = XAieGbl_LookupConfig(XPAR_AIE_DEVICE_ID);
+  // XAieGbl_CfgInitialize(&AieInst, &TileInst[0][0], AieConfigPtr);
+}
+
+}
 
 namespace {
 
@@ -23,7 +70,7 @@ int queue_create(uint32_t size, queue_t **queue)
   uint64_t queue_address[1] = {base_address + sizeof(dispatch_packet_t)};
   uint64_t queue_base_address[1] = {queue_address[0] + sizeof(dispatch_packet_t)};
 
-  xil_printf("setup_queue, %x bytes + %d 64 byte packets\n", sizeof(queue_t), size);
+  xil_printf("setup_queue, %x bytes + %d 64 byte packets\n\r", sizeof(queue_t), size);
 
   // The address of the queue_t is stored @ base_address
   memcpy((void*)base_address, (void*)queue_address, sizeof(uint64_t));
@@ -55,38 +102,23 @@ int queue_create(uint32_t size, queue_t **queue)
   return 0;
 }
 
-uint64_t queue_load_read_index(queue_t *q)
+void complete_agent_dispatch_packet(dispatch_packet_t *pkt)
 {
-  return q->read_index;
+  // completion phase
+  packet_set_active(pkt, false);
+  signal_subtract_acq_rel((signal_t*)&pkt->completion_signal, 1);
 }
 
-uint64_t queue_add_write_index(queue_t *q)
+void handle_agent_dispatch_packet(dispatch_packet_t *pkt)
 {
-  return q->write_index++;
-}
-
-uint64_t queue_load_write_index(queue_t *q)
-{
-  return q->write_index;
-}
-
-bool packet_get_active(dispatch_packet_t *pkt)
-{
-  return pkt->reserved1 & 0x1;
-}
-
-void packet_set_active(dispatch_packet_t *pkt, bool b)
-{
-  pkt->reserved1 = (pkt->reserved1 & ~0x1) | b;
-}
-
-void handle_agent_dispatch(dispatch_packet_t *pkt)
-{
-  packet_set_active(pkt, true);
+  // get the index
   uint32_t pkt_idx;
   memcpy(&pkt_idx, &pkt, sizeof(pkt_idx));
   pkt_idx = ((pkt_idx & 0x3fff) >> 6) - 2;
-  xil_printf("handle agent dispatch pkt %x\n", pkt_idx);
+  xil_printf("handle agent dispatch pkt %x @ 0x%llx\n\r", pkt_idx, (size_t)pkt);
+
+  // packet is in active phase
+  packet_set_active(pkt, true);
 }
 
 } // namespace
@@ -94,36 +126,36 @@ void handle_agent_dispatch(dispatch_packet_t *pkt)
 int main()
 {
   init_platform();
+  xaie_init();
 
   queue_t *q = nullptr;
   queue_create(MB_QUEUE_SIZE, &q);
-  xil_printf("Created queue @ 0x%llx\n", (size_t)q);
+  xil_printf("Created queue @ 0x%llx\n\r", (size_t)q);
   bool done = false;
   int cnt = 0;
   while (!done) {
     if (!(cnt++ % 0x00100000))
-      xil_printf("No Ding Dong 0x%llx\n", q->doorbell);
+      xil_printf("No Ding Dong 0x%llx\n\r", q->doorbell);
 
     if (q->doorbell+1 > q->last_doorbell) {
-      xil_printf("Ding Dong 0x%llx\n", q->doorbell);
+      xil_printf("Ding Dong 0x%llx\n\r", q->doorbell);
 
       q->last_doorbell = q->doorbell+1;
 
       auto rd_idx = queue_load_read_index(q);
-      auto wr_idx = queue_load_write_index(q);
-      for (; rd_idx < wr_idx; rd_idx++) {
-        xil_printf("Handle pkts\n");
-        dispatch_packet_t *pkt = &((dispatch_packet_t*)q->base_address)[rd_idx % q->size];
-        switch (pkt->header & 0xff) {
-          default:
-          case HSA_PACKET_TYPE_INVALID:
-            break;
-          case HSA_PACKET_TYPE_AGENT_DISPATCH:
-            handle_agent_dispatch(pkt);
-            break;
-        }
-      }
+      xil_printf("Handle pkt read_index=%d\n\r", rd_idx);
 
+      dispatch_packet_t *pkt = &((dispatch_packet_t*)q->base_address)[rd_idx % q->size];
+      switch (pkt->type) {
+        default:
+        case HSA_PACKET_TYPE_INVALID:
+          break;
+        case HSA_PACKET_TYPE_AGENT_DISPATCH:
+          handle_agent_dispatch_packet(pkt);
+          complete_agent_dispatch_packet(pkt);
+          queue_add_read_index(q, 1);
+          break;
+      }
     }
   }
 
