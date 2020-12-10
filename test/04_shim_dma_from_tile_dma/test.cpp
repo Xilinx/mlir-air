993d3df9a3c514761f01ffe4e008aea7b8fd68b8
Phil James-Roxby <pjr@xilinx.com>
2020-12-10 11:20:55 -0700

    [TEST] Fix up test 4 with manual (for now) aie_inc.cpp and correct sizing on shim DMA

---
+++ test/04_shim_dma_from_tile_dma/test.cpp
@@ -0,0 +1,140 @@
+// (c) Copyright 2020 Xilinx Inc. All Rights Reserved.
+
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <thread>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <xaiengine.h>
+
+#define XAIE_NUM_ROWS            8
+#define XAIE_NUM_COLS           50
+#define XAIE_ADDR_ARRAY_OFF     0x800
+
+#define HIGH_ADDR(addr)	((addr & 0xffffffff00000000) >> 32)
+#define LOW_ADDR(addr)	(addr & 0x00000000ffffffff)
+
+namespace {
+
+XAieGbl_Config *AieConfigPtr;	                          /**< AIE configuration pointer */
+XAieGbl AieInst;	                                      /**< AIE global instance */
+XAieGbl_HwCfg AieConfig;                                /**< AIE HW configuration instance */
+XAieGbl_Tile TileInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];  /**< Instantiates AIE array of [XAIE_NUM_COLS] x [XAIE_NUM_ROWS] */
+XAieDma_Tile TileDMAInst[XAIE_NUM_COLS][XAIE_NUM_ROWS+1];
+
+#include "aie_inc.cpp"
+
+}
+
+int
+main(int argc, char *argv[])
+{
+  auto col = 7;
+
+  size_t aie_base = XAIE_ADDR_ARRAY_OFF << 14;
+  XAIEGBL_HWCFG_SET_CONFIG((&AieConfig), XAIE_NUM_ROWS, XAIE_NUM_COLS, XAIE_ADDR_ARRAY_OFF);
+  XAieGbl_HwInit(&AieConfig);
+  AieConfigPtr = XAieGbl_LookupConfig(XPAR_AIE_DEVICE_ID);
+  XAieGbl_CfgInitialize(&AieInst, &TileInst[0][0], AieConfigPtr);
+
+  // reset cores and locks
+  for (int i = 1; i <= XAIE_NUM_ROWS; i++) {
+    for (int j = 0; j < XAIE_NUM_COLS; j++) {
+      XAieTile_CoreControl(&(TileInst[j][i]), XAIE_DISABLE, XAIE_ENABLE);
+      for (int l=0; l<16; l++)
+        XAieTile_LockRelease(&(TileInst[j][i]), l, 0x0, 0);
+    }
+  }
+
+  // cores
+  //
+  //  mlir_initialize_cores();
+  mlir_configure_cores();
+  //XAieTile_CoreControl(&(TileInst[col][1]), XAIE_DISABLE, XAIE_DISABLE);
+  //XAieTile_CoreControl(&(TileInst[col][2]), XAIE_DISABLE, XAIE_DISABLE);
+
+  //XAieTile_ShimColumnReset(&(TileInst[col][0]), XAIE_RESETENABLE);
+  //XAieTile_ShimColumnReset(&(TileInst[col][0]), XAIE_RESETDISABLE);
+
+  // configure switchboxes
+  //
+  mlir_configure_switchboxes();
+  
+  // locks
+  //
+  mlir_initialize_locks();
+
+  // dmas
+  //
+
+  mlir_configure_dmas();
+
+  XAieDma_Shim ShimDmaInst1;
+  uint32_t *bram_ptr;
+
+  #define BRAM_ADDR 0x020100000000LL
+  #define DMA_COUNT 512
+
+  int fd = open("/dev/mem", O_RDWR | O_SYNC);
+  if (fd != -1) {
+    bram_ptr = (uint32_t *)mmap(NULL, 0x8000, PROT_READ|PROT_WRITE, MAP_SHARED, fd, BRAM_ADDR);
+    for (int i=0; i<DMA_COUNT; i++) {
+      bram_ptr[i] = 0xdeadbeef;
+      //printf("%p %llx\n", &bram_ptr[i], bram_ptr[i]);
+    }
+  }
+
+  for (int i=0; i<DMA_COUNT; i++) {
+    uint32_t d = i+1;
+    XAieTile_DmWriteWord(&(TileInst[col][2]), i*4, d);
+  }
+
+
+  auto burstlen = 4;
+  XAieDma_ShimInitialize(&(TileInst[col][0]), &ShimDmaInst1);
+  XAieDma_ShimBdSetAddr(&ShimDmaInst1, 1, HIGH_ADDR((u64)BRAM_ADDR), LOW_ADDR((u64)BRAM_ADDR), sizeof(u32) * DMA_COUNT);
+  XAieDma_ShimBdSetAxi(&ShimDmaInst1, 1 , 0, burstlen, 0, 0, XAIE_ENABLE);
+  XAieDma_ShimBdWrite(&ShimDmaInst1, 1);
+  XAieDma_ShimSetStartBd((&ShimDmaInst1), XAIEDMA_SHIM_CHNUM_S2MM0, 1);
+
+  auto ret = XAieDma_ShimPendingBdCount(&ShimDmaInst1, XAIEDMA_SHIM_CHNUM_S2MM0);
+  if (ret)
+    printf("%s %d Warn %d\n", __FUNCTION__, __LINE__, ret);
+
+  XAieDma_ShimChControl((&ShimDmaInst1), XAIEDMA_SHIM_CHNUM_S2MM0, XAIE_DISABLE, XAIE_DISABLE, XAIE_ENABLE);
+
+  // We wrote data, so lets toggle locks 0 and 1
+  XAieTile_LockRelease(&(TileInst[col][2]), 0, 0x1, 0);
+  XAieTile_LockRelease(&(TileInst[col][2]), 1, 0x1, 0);
+
+  auto count = 0;
+  while (XAieDma_ShimPendingBdCount(&ShimDmaInst1, XAIEDMA_SHIM_CHNUM_S2MM0)) {
+    XAieLib_usleep(1000);
+    count++;
+    if (!(count % 1000)) {
+      printf("%d seconds\n",count/1000);
+      if (count == 2000) break;
+    }
+  }
+
+  int errors = 0;
+  for (int i=0; i<DMA_COUNT; i++) {
+    uint32_t d = bram_ptr[i];
+    if (d != (i+1)) {
+      errors++;
+      printf("mismatch %x != 1 + %x\n", d, i);
+    }
+  }
+
+  if (!errors) {
+    printf("PASS!\n");
+  }
+  else {
+    printf("fail %d/%d.\n", (DMA_COUNT-errors), DMA_COUNT);
+  }
+
+}
