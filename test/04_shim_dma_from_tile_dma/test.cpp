c45f6382ef5ced9350825b2867e93317057a7ce6
Jeff Fifield <jeff.fifield@xilinx.com>
2020-12-15 13:15:32 -0700

    Don't stomp on the microblaze queue, delete old test

--- test/04_shim_dma_from_tile_dma/test.cpp
+++ test/04_shim_dma_from_tile_dma/test.cpp
@@ -76,7 +76,7 @@ main(int argc, char *argv[])
   XAieDma_Shim ShimDmaInst1;
   uint32_t *bram_ptr;
 
-  #define BRAM_ADDR 0x020100000000LL
+  #define BRAM_ADDR (0x4000+0x020100000000LL)
   #define DMA_COUNT 512
 
   int fd = open("/dev/mem", O_RDWR | O_SYNC);
