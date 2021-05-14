2c8bfb057ae60f46ed00dafb78c211688a8326a4
Louis Coulon <lcoulon@xilinx.com>
2021-05-14 17:42:58 +0100

    started to explore plotting with exhaustive search

---
+++ herds/cnv2/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='Plots paths')
+parser.add_argument('-filename', default="", help='filename to be plotted')
+args = parser.parse_args()
+
+labels = np.genfromtxt(args.filename, delimiter=" ", usecols=0, dtype=str, skip_header=1)
+data = np.genfromtxt(args.filename, delimiter=' ', usecols=[1,2,3,4,5,6,7,8,9,10], names=True, dtype=int)
+
+def plotTimes():
+    plt.plot(data['Compute'], label='ComputeTime')
+    plt.plot(data['ActCommunication'], label='ActCom')
+    plt.plot(data['WeightCommunication'], label='WeightCom')
+    plt.xticks(np.arange(len(labels)), labels, rotation='vertical')
+    plt.legend()
+    plt.ylabel("cycles")
+    plt.show()
+
+plotTimes()
+
