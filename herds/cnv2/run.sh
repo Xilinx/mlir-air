85c66e1c107cb8a903522865b0882e57c671487a
Kristof Denolf <kristof@xilinx.com>
2021-07-30 14:52:24 -0700

    update with last changes from Louis

--- herds/cnv2/run.sh
+++ herds/cnv2/run.sh
@@ -1,1 +1,2 @@
-python3 $1.py > $1.mlir; air-opt --aten-to-air $1.mlir > $1.air.mlir; air-opt --air-name-layers $1.air.mlir > $1.air_named.mlir; aten-opt --air-expand-graph $1.air_named.mlir
+#python3 $1.py > $1.mlir; air-opt --aten-to-air $1.mlir > $1.air.mlir; air-opt --air-name-layers $1.air.mlir > $1.air_named.mlir; aten-opt --air-expand-graph $1.air_named.mlir
+python3 $1.py > $1.mlir; aten-opt --aten-to-air $1.mlir > $1.air.mlir; aten-opt --air-name-layers $1.air.mlir > $1.air_named.mlir; aten-opt --air-expand-graph $1.air_named.mlir
