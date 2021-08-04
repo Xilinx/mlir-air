5959b7ef7bb618e13c4dcd34d4fa050deaf100d1
Jack Lo <jackl@xilinx.com>
2021-08-04 09:56:29 -0700

    Fixed lit path for aie and air tools.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -75,6 +75,8 @@ config.air_tools_dir = os.path.join(config.air_obj_root, 'bin')
 
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
+llvm_config.with_environment('PATH', config.aie_tools_dir, append_path=True)
+llvm_config.with_environment('PATH', config.air_tools_dir, append_path=True)
 
 tool_dirs = [config.aie_tools_dir, config.air_tools_dir, config.llvm_tools_dir]
 tools = [
