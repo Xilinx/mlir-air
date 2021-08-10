2427648db51d8a384837cf73564c5176ba4ef139
Jeff Fifield <jeff.fifield@xilinx.com>
2021-08-10 13:38:29 -0600

    minor cleanup

--- herds/mmult_i32_2x2/mmult.py
+++ herds/mmult_i32_2x2/mmult.py
@@ -6,26 +6,14 @@ from air.compiler.jit import Compiler
 
 t0 = torch.randint(high=10, size=(128,128), dtype=torch.int32)
 t1 = torch.randint(high=10, size=(128,128), dtype=torch.int32)
-t3 = torch.randint(high=10, size=(128,128), dtype=torch.int32)
 
 builder = torch_mlir.ModuleBuilder()
 with builder.capture_function("task", [t0,t1]) as f:
     t2 = torch.mm(t0, t1)
-    #t4 = torch.mm(t2, t3)
     f.returns([t2])
 t2_mlir = builder.module
 #print(builder.module)
 
 c = Compiler()
 m = c.torch_to_aten(t2_mlir)
-#m = c.aten_to_linalg(m)
 print(m)
-
-#c.air_linalg_codegen(m)
-#print(m)
-
-#c.affine_to_air(m)
-#print(m)
-
-#c.air_to_aie(m, row_offset=2, col_offset=7)
-#print(m)
