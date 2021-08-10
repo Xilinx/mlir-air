87a09bb8ae1d82d11ca9db839adfd410391cac7c
Jeff Fifield <jeff.fifield@xilinx.com>
2021-08-10 06:39:35 -0600

    Rename aten-to-air to aten-to-xten

--- herds/cnv2/run_ca.sh
+++ herds/cnv2/run_ca.sh
@@ -1,1 +1,1 @@
-python cnv2_ca.py > ca.mlir; air-opt --aten-to-air ca.mlir > ca.air.mlir; air-opt --air-name-layers ca.air.mlir > ca.air_named.mlir; aten-opt --air-expand-graph ca.air_named.mlir
+python cnv2_ca.py > ca.mlir; air-opt --aten-to-xten ca.mlir > ca.air.mlir; air-opt --air-name-layers ca.air.mlir > ca.air_named.mlir; aten-opt --air-expand-graph ca.air_named.mlir
--- herds/two_filter_2d/run.sh
+++ herds/two_filter_2d/run.sh
@@ -1,1 +1,1 @@
-python3 $1.py; air-opt --aten-to-air $1.mlir > $1.air.mlir; air-opt --air-name-layers $1.air.mlir > $1.air_named.mlir; aten-opt --air-expand-graph $1.air_named.mlir
+python3 $1.py; air-opt --aten-to-xten $1.mlir > $1.air.mlir; air-opt --air-name-layers $1.air.mlir > $1.air_named.mlir; aten-opt --air-expand-graph $1.air_named.mlir
