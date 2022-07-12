2833f84c8fd0c9b3a43fe086f35a72de70732698
Erwei Wang <erweiw@xilinx.com>
2022-07-12 15:16:01 -0700

    Removing redundant ops in dma hoisting

--- mlir/lib/Transform/AIRDependencyScheduleOpt.cpp
+++ mlir/lib/Transform/AIRDependencyScheduleOpt.cpp
@@ -102,22 +102,17 @@ namespace {
             // Note: moving consts of which op_1 depends on AFTER op_2 to maintain dominance if consts are shared by both
             for (auto op_2_operand : op_2->getOperands()){
               if (op_2_operand.getDefiningOp() && isa<arith::ConstantOp>(op_2_operand.getDefiningOp())){
-                // op_2_operand.getDefiningOp()->moveBefore(op_2);
                 rewriter.setInsertionPoint(op_2);
                 auto newConstOp = rewriter.clone(*op_2_operand.getDefiningOp());
-                op_2->moveAfter(newConstOp);
               }
             }
             // Move const ops which produce op_1 operands
             for (auto op_1_operand : op_1->getOperands()){
               if (op_1_operand.getDefiningOp() && isa<arith::ConstantOp>(op_1_operand.getDefiningOp())){
-                // op_1_operand.getDefiningOp()->moveBefore(op_1);
                 rewriter.setInsertionPoint(op_1);
                 auto newConstOp = rewriter.clone(*op_1_operand.getDefiningOp());
-                op_1->moveAfter(newConstOp);
               }
             }
-            // return success();
           }
         }
         if (foundDmaPairForThisOp2) continue; // Ensure unique pairing
