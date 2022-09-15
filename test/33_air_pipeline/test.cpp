4e19b4c11ca4196e55d97bcd9a9272441dfbce4a
Jeff Fifield <jeff.fifield@xilinx.com>
2022-09-15 15:51:41 -0600

    Fix test to use queue + clang-format

--- test/33_air_pipeline/test.cpp
+++ test/33_air_pipeline/test.cpp
@@ -4,36 +4,35 @@
 #include <cmath>
 #include <cstdio>
 #include <cstring>
-#include <thread>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <dlfcn.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <thread>
+#include <unistd.h>
 
 #include <xaiengine.h>
 
 #include "air_host.h"
 #include "air_tensor.h"
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   auto col = 3;
   auto row = 3;
 
-  /*aie_libxaie_ctx_t *xaie = */air_init_libxaie1();
+  /*aie_libxaie_ctx_t *xaie = */ air_init_libxaie1();
 
-  //// create the queue
-  //queue_t *q = nullptr;
-  //auto ret = air_queue_create(MB_QUEUE_SIZE, HSA_QUEUE_TYPE_SINGLE, &q, AIR_VCK190_SHMEM_BASE);
-  //assert(ret == 0 && "failed to create queue!");
+  // create the queue
+  queue_t *q = nullptr;
+  auto ret = air_queue_create(MB_QUEUE_SIZE, HSA_QUEUE_TYPE_SINGLE, &q,
+                              AIR_VCK190_SHMEM_BASE);
+  assert(ret == 0 && "failed to create queue!");
 
-  #define DATA_LENGTH 1024
-  #define DATA_TYPE int
+#define DATA_LENGTH 1024
+#define DATA_TYPE int
 
-  tensor_t<DATA_TYPE,1> input_a, input_b;
-  tensor_t<DATA_TYPE,1> output;
+  tensor_t<DATA_TYPE, 1> input_a, input_b;
+  tensor_t<DATA_TYPE, 1> output;
   input_a.shape[0] = DATA_LENGTH;
   input_a.alloc = input_a.data =
       (DATA_TYPE *)malloc(sizeof(DATA_TYPE) * input_a.shape[0]);
@@ -46,7 +45,7 @@ main(int argc, char *argv[])
   output.alloc = output.data =
       (DATA_TYPE *)malloc(sizeof(DATA_TYPE) * output.shape[0]);
   DATA_TYPE d = 1;
-  for (int i=0; i<input_a.shape[0]; i++) {
+  for (int i = 0; i < input_a.shape[0]; i++) {
     input_a.data[i] = d;
     input_b.data[i] = ((DATA_TYPE)DATA_LENGTH) + d;
     output.data[i] = -1;
@@ -54,17 +53,18 @@ main(int argc, char *argv[])
   }
 
   printf("loading aie_ctrl.so\n");
-  auto handle = air_module_load_from_file(nullptr);
+  auto handle = air_module_load_from_file(nullptr, q);
   assert(handle && "failed to open aie_ctrl.so");
 
-  auto launch = (void (*)(void*,void *,void *))dlsym((void*)handle, "_mlir_ciface_launch");
+  auto launch = (void (*)(void *, void *, void *))dlsym((void *)handle,
+                                                        "_mlir_ciface_launch");
   assert(launch && "failed to locate _mlir_ciface_launch in .so");
 
-  launch((void*)&input_a, (void*)&input_b, (void*)&output);
+  launch((void *)&input_a, (void *)&input_b, (void *)&output);
 
   int errors = 0;
 
-  for (int i=0;i<DATA_LENGTH;i++) {
+  for (int i = 0; i < DATA_LENGTH; i++) {
     DATA_TYPE ref = (input_a.data[i] * input_b.data[i]) + (DATA_TYPE)1 +
                     (DATA_TYPE)2 + (DATA_TYPE)3;
     if (output.data[i] != ref) {
@@ -80,9 +80,8 @@ main(int argc, char *argv[])
   if (!errors) {
     printf("PASS!\n");
     return 0;
-  }
-  else {
-    printf("fail %d/%d.\n", (DATA_LENGTH-errors), DATA_LENGTH);
+  } else {
+    printf("fail %d/%d.\n", (DATA_LENGTH - errors), DATA_LENGTH);
     return -1;
   }
 }
