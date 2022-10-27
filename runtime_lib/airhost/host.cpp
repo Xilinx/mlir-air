3dd82f6997d10e194ba30fe8e01f6a36b72d4c0e
Joseph Melber <jmelber@xilinx.com>
2022-10-27 15:33:46 -0600

    Move and add license for get_pci_dbdf

--- runtime_lib/airhost/host.cpp
+++ runtime_lib/airhost/host.cpp
@@ -24,6 +24,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "air_host.h"
+#ifdef AIR_PCIE
+#include "utility.hpp"
+#endif
 
 #include <assert.h>
 #include <dlfcn.h>
@@ -32,7 +35,6 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
-#include <vector>
 
 #include <dirent.h>
 #include <fstream> // ifstream
@@ -362,72 +364,11 @@ uint64_t air_herd_load(const char *name) {
 }
 
 #ifdef AIR_PCIE
-// Copied from
-// https://github.com/andre-richter/easy-pci-mmap/blob/master/utility.cpp
-// TODO: This is from an MIT license need to get the proper licenses in check
-void get_pci_dbdf(std::vector<std::string> *bdf_vect, uint32_t vendor,
-                  uint32_t device, unsigned int func_num) {
-  DIR *dir;
-  std::string err = std::string();
-
-  if (bdf_vect == NULL) {
-    printf("Passed NULL bdf_vect\n");
-    return;
-  }
-
-  if ((dir = opendir("/sys/bus/pci/devices")) == nullptr) {
-    printf("Cannot open directory /sys/bus/pci/devices\n");
-    return;
-  }
-
-  // iterate over all PCIe devices
-  struct dirent *d;
-  std::ifstream ifstr;
-  std::string bdf_found;
-
-  while ((d = readdir(dir)) != nullptr) {
-
-    // only consider actual device folders, not ./ and ../
-    if (strstr(d->d_name, "0000:") != nullptr) {
-      bdf_found = std::string(d->d_name);
-
-      // Continue only if the function number matches
-      if ((unsigned int)(bdf_found.back() - '0') != func_num)
-        continue;
-
-      std::string path("/sys/bus/pci/devices/" + bdf_found);
-
-      // read vendor id
-      ifstr.open(path + "/vendor");
-      std::string tmp((std::istreambuf_iterator<char>(ifstr)),
-                      std::istreambuf_iterator<char>());
-      ifstr.close();
-
-      // check if vendor id is correct
-      if (std::stoul(tmp, nullptr, 16) == vendor) {
-
-        // read device id
-        ifstr.open(path + "/device");
-        std::string tmp((std::istreambuf_iterator<char>(ifstr)),
-                        std::istreambuf_iterator<char>());
-        ifstr.close();
-
-        // check if device also fits
-        if (std::stoul(tmp, nullptr, 16) == device)
-          bdf_vect->push_back("/sys/bus/pci/devices/" + bdf_found +
-                              "/resource");
-      }
-    }
-  }
-
-  return;
-}
-
 hsa_status_t air_get_physical_devices() {
 
   // Right now just hardcode VCK5000 BDF
   std::vector<std::string> bdf_vect;
-  get_pci_dbdf(&bdf_vect, 0x000010ee, 0x0000b034, 0);
+  utility::get_pci_dbdf(&bdf_vect, 0x000010ee, 0x0000b034, 0);
 
   struct stat st;
   for (std::string iter : bdf_vect) {
