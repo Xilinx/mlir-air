833131825225ba639672f365d83c2e63ddd15261
Eddie Richter <eddier@xilinx.com>
2022-10-27 12:08:04 -0600

    clang-format

--- runtime_lib/airhost/include/air_network.h
+++ runtime_lib/airhost/include/air_network.h
@@ -1,4 +1,5 @@
-//===- air_network.h ---------------------------------------------*- C++ -*-===//
+//===- air_network.h ---------------------------------------------*- C++
+//-*-===//
 //
 // Copyright (C) 2020-2022, Xilinx Inc.
 // Copyright (C) 2022, Advanced Micro Devices, Inc.
@@ -27,8 +28,8 @@
 #define AIR_NETWORK_H
 
 #include "acdc_queue.h"
-#include "hsa_defs.h"
 #include "air_tensor.h"
+#include "hsa_defs.h"
 
 //#include "test_library.h"
 
@@ -50,14 +51,19 @@ struct world_view_entry {
 // RDMA operations and variables
 hsa_status_t air_set_hostname(char hostname[100]);
 hsa_status_t air_get_hostname(char hostname[100]);
-hsa_status_t air_explore_world(uint32_t ernic_id, uint64_t dev_mem_offset, uint64_t bar_offset);
+hsa_status_t air_explore_world(uint32_t ernic_id, uint64_t dev_mem_offset,
+                               uint64_t bar_offset);
 hsa_status_t air_ernic_free();
-hsa_status_t air_ernic_mem_alloc(char buff_name[100], uint32_t size, void *t, bool register_mem);
+hsa_status_t air_ernic_mem_alloc(char buff_name[100], uint32_t size, void *t,
+                                 bool register_mem);
 
-void air_recv(signal_t *s, tensor_t<uint32_t, 1> *t, uint32_t size, uint32_t offset, uint32_t src_rank, queue_t *q, uint8_t ernic_sel);
-void air_send(signal_t *s, tensor_t<uint32_t, 1> *t, uint32_t size, uint32_t offset, uint32_t dst_rank, queue_t *q, uint8_t ernic_sel);
-void air_barrier(tensor_t<uint32_t, 1> *dummy_tensor, queue_t *q, uint8_t ernic_sel);
-
-
+void air_recv(signal_t *s, tensor_t<uint32_t, 1> *t, uint32_t size,
+              uint32_t offset, uint32_t src_rank, queue_t *q,
+              uint8_t ernic_sel);
+void air_send(signal_t *s, tensor_t<uint32_t, 1> *t, uint32_t size,
+              uint32_t offset, uint32_t dst_rank, queue_t *q,
+              uint8_t ernic_sel);
+void air_barrier(tensor_t<uint32_t, 1> *dummy_tensor, queue_t *q,
+                 uint8_t ernic_sel);
 
 #endif
