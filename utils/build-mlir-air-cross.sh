9232acfa253a95b6b91c7e0f3c894a04cedd5f9c
Jack Lo <jackl@xilinx.com>
2022-10-27 22:20:41 -0700

    Updated build scripts and added cross compile scripts.

--- utils/build-mlir-air-cross.sh
+++ utils/build-mlir-air-cross.sh
@@ -30,14 +30,16 @@
 # <cmakeModules dir>, and <mlir-aie dir>. Assuming they are all in the same
 # subfolder, it would look like:
 #
-# build-mlir-air.sh <sysroot dir> <llvm dir> <cmakeModules dir> <mlir-aie dir>
-#     <mlir-air dir> <build dir> <install dir>
+# build-mlir-air.sh <sysroot dir> <gcc version> <llvm dir> <cmakeModules dir> 
+#     <mlir-aie dir> <mlir-air dir> <build dir> <install dir>
 #
-# e.g. build-mlir-air.sh /scratch/vck190_bare_prod_sysroot /scratch/llvm 
+# e.g. build-mlir-air.sh /scratch/vck190_bare_prod_sysroot 10.2.0 /scratch/llvm 
 #          /scratch/mlir-aie
 #          /scratch/cmakeModules/cmakeModulesXilinx
 #
 # <sysroot dir>      - sysroot, absolute directory
+# <gcc version>      - gcc version in sysroot (needed in many petalinux 
+#                      sysroots to find imporant libs)
 # <cmakeModules dir> - cmakeModules, absolute directory
 # <mlir-aie dir>     - mlir-aie, absolute directory
 #
@@ -47,19 +49,20 @@
 #
 ##===----------------------------------------------------------------------===##
 
-if [ "$#" -lt 4 ]; then
-    echo "ERROR: Needs at least 4 arguments for <sysroot dir>, <llvm dir>,"
-    echo "<cmakeModules dir> and <mlir-aie dir>."
+if [ "$#" -lt 5 ]; then
+    echo "ERROR: Needs at least 4 arguments for <sysroot dir>, <llvm dir>, "
+	echo "<cmakeModules dir> and <mlir-aie dir>."
     exit 1
 fi
 SYSROOT_DIR=$1
-LLVM_DIR=$2
-CMAKEMODULES_DIR=$3
-MLIR_AIE_DIR=$4
+GCC_VER=$2
+LLVM_DIR=$3
+CMAKEMODULES_DIR=$4
+MLIR_AIE_DIR=$5
 
-MLIR_AIR_DIR=${5:-"mlir-air"}
-BUILD_DIR=${6:-"build"}
-INSTALL_DIR=${7:-"install"}
+MLIR_AIR_DIR=${6:-"mlir-air"}
+BUILD_DIR=${7:-"build"}
+INSTALL_DIR=${8:-"install"}
 
 mkdir -p $MLIR_AIR_DIR/$BUILD_DIR
 mkdir -p $MLIR_AIR_DIR/$INSTALL_DIR
@@ -69,12 +72,11 @@ PYTHON_ROOT=`pip3 show pybind11 | grep Location | awk '{print $2}'`
 
 cmake .. \
 	-GNinja \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_INSTALL_PREFIX=../install \
+	-DCMAKE_INSTALL_PREFIX=../${INSTALL_DIR} \
 	-DCMAKE_TOOLCHAIN_FILE_OPT=${CMAKEMODULES_DIR}/toolchain_clang_crosscomp_arm_petalinux.cmake \
-	-DArch=arm64 \
-	-DgccVer=10.2.0 \
+    -DSysroot=${SYSROOT_DIR} \
+    -DArch=arm64 \
+    -DgccVer=${GCC_VER} \
 	-DCMAKE_USE_TOOLCHAIN=FALSE \
 	-DCMAKE_USE_TOOLCHAIN_AIRHOST=TRUE \
 	-DLLVM_DIR=${LLVM_DIR}/build/lib/cmake/llvm \
