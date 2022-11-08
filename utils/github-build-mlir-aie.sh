1dd60a917797fc2d09d856b15525130ce4bd0a80
Joseph Melber <jgmelber@gmail.com>
2022-11-08 13:01:48 -0700

    Update and rename github-clone-build-mlir-aie.sh to github-build-mlir-aie.sh

--- utils/github-build-mlir-aie.sh
+++ utils/github-build-mlir-aie.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-##===- utils/github-clone-build-mlir-aie.sh -----------------*- Script -*-===##
+##===- utils/github-build-mlir-aie.sh -----------------*- Script -*-===##
 # 
 # Copyright (C) 2022, Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: MIT
 
 ##===----------------------------------------------------------------------===##
 #
-# This script checks out and builds a specific version of mlir-aie.
+# This script builds a specific version of mlir-aie.
 #
 # This script is intended to be called from the github workflows.
 #
@@ -16,14 +16,6 @@
 MLIR_AIE_DIR="mlir-aie"
 BUILD_DIR="build"
 INSTALL_DIR="install"
-
-HASH=ad2cbf5366a2470ae6b543cff53e80bed1179d79
-
-git clone --depth 1 https://github.com/Xilinx/mlir-aie.git $MLIR_AIE_DIR
-pushd $MLIR_AIE_DIR
-git fetch --depth=1 origin $HASH
-git checkout $HASH
-popd
 
 mkdir -p $MLIR_AIE_DIR/$BUILD_DIR
 mkdir -p $MLIR_AIE_DIR/$INSTALL_DIR
