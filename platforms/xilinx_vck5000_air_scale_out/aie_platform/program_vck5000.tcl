90dd9267609536b6a5ad56d02c76ec7f03c4aa45
Joseph Melber <jmelber@xilinx.com>
2022-11-08 15:38:32 -0700

    Update vck5000 programming script
      * support for hosts with multiple vck5000 PCIe cards installed
      * programming script now asks for a device index
      * for hosts with only one device installed the index is 0

--- platforms/xilinx_vck5000_air_scale_out/aie_platform/program_vck5000.tcl
+++ platforms/xilinx_vck5000_air_scale_out/aie_platform/program_vck5000.tcl
@@ -19,10 +19,24 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+puts "Please enter the card index to program: "
+
+set data [gets stdin]
+scan $data "%d" myidx
+
+# Open vivado hardware manager to program device
 open_hw_manager
+# Initiate harwdare manager's server
 connect_hw_server -url localhost:3121
+# Set the current target card based on index 0->N
+current_hw_target [lindex [get_hw_targets] $myidx]
+# Open the current target card
 open_hw_target
-current_hw_device [get_hw_devices xcvc1902_1]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xcvc1902_1] 0]
-set_property PROGRAM.FILE {./final_vck5000.pdi} [get_hw_devices xcvc1902_1]
-program_hw_devices [get_hw_devices xcvc1902_1]
+# Set the current target device. ID 1 for xcvc1902_*
+current_hw_device [lindex [get_hw_devices] 1]
+# Refresht the device and setup probes because that's what the GUI does
+refresh_hw_device -update_hw_probes false [lindex [lindex [get_hw_devices] 1] 0]
+# Set the programming file to the final PDI with the firmware
+set_property PROGRAM.FILE {./final_vck5000.pdi} [lindex [get_hw_devices] 1]
+# Initiate device programming
+program_hw_devices [lindex [get_hw_devices] 1]
