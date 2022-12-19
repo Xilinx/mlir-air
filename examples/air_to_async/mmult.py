6f1231b679b8385aa6e8322ec2c45225b1cee513
Jeff Fifield <jeff.fifield@xilinx.com>
2022-12-19 15:29:30 -0700

    Add an e2e air-to-async example

---
+++ examples/air_to_async/mmult.py
@@ -0,0 +1,29 @@
+# mmult.py -*- Python -*-
+#
+# Copyright (C) 2022, Xilinx Inc. All rights reserved.
+# Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+import torch
+import torch_mlir
+
+shape = [128,128]
+dtype = torch.int32
+class model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, t0, t1, t2):
+        t3 = torch.mm(t0, t1)
+        t4 = torch.mm(t3, t2)
+        return t4
+
+program = model()
+
+module = torch_mlir.compile(
+    program,
+    (torch.ones(shape, dtype=dtype), torch.ones(shape, dtype=dtype), torch.ones(shape, dtype=dtype)),
+    output_type=torch_mlir.OutputType.LINALG_ON_TENSORS
+)
+
+print(module)
