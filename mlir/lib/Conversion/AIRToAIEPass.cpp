e1e0b8cb61912f231c77ecdb34da974a004b79cb
abisca <andrab@amd.com>
2023-02-22 06:10:40 -0700

    Reverted more changes.

--- mlir/lib/Conversion/AIRToAIEPass.cpp
+++ mlir/lib/Conversion/AIRToAIEPass.cpp
@@ -152,21 +152,20 @@ struct DMAAllocator {
   AIE::TileOp getTile(ModuleOp aie_module, air::DmaMemcpyInterface &dmaOp,
                       int64_t tile_channel, int64_t col, int64_t row) {
     auto src_memory_space =
-      dmaOp.getSrcMemref().getType().cast<MemRefType>().getMemorySpaceAsInt();
+        dmaOp.getSrcMemref().getType().cast<MemRefType>().getMemorySpaceAsInt();
     auto dst_memory_space =
-      dmaOp.getDstMemref().getType().cast<MemRefType>().getMemorySpaceAsInt();
-    auto dmaID = dmaOp.getId();
-    
+        dmaOp.getDstMemref().getType().cast<MemRefType>().getMemorySpaceAsInt();
+
     bool isMM2S = (src_memory_space < dst_memory_space);
     auto allocs = isMM2S ? &mm2s_allocs : &s2mm_allocs;
 
     for (auto &t : *allocs) {
       if (col == t.col && row == t.row) {
         for (auto id : t.dma_id)
-          if (dmaID == id)
+          if (dmaOp.getId(); == id)
             return t.dma_tile;
         if (tile_channel == t.tile_channel) {
-          t.dma_id.push_back(dmaID);
+          t.dma_id.push_back(dmaOp.getId(););
           return t.dma_tile;
         }
       }
@@ -179,8 +178,8 @@ struct DMAAllocator {
                        row,
                        (int64_t)dma_channel,
                        tile_channel,
-                       {dmaID}});
-    LLVM_DEBUG(llvm::outs() << "isMM2S = " << isMM2S << " " << dmaID
+                       {dmaOp.getId();}});
+    LLVM_DEBUG(llvm::outs() << "isMM2S = " << isMM2S << " " << dmaOp.getId();
                             << ", col =" << col << ", row = " << row
                             << ", l2 col =" << dma_col
                             << ", l2 chan =" << dma_channel << "\n");
