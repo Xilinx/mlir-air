673423fa35e8093b89ff18bfc6a9d24f1248efd7
Jeff Fifield <jeff.fifield@amd.com>
2023-03-06 10:40:24 -0700

    Remove air-to-cpu pass (replaced by air-to-async)

--- mlir/include/air/Conversion/Passes.h
+++ mlir/include/air/Conversion/Passes.h
@@ -13,7 +13,6 @@
 #include "air/Conversion/AIRRtToLLVMPass.h"
 #include "air/Conversion/AIRToAIEPass.h"
 #include "air/Conversion/AIRToAsyncPass.h"
-#include "air/Conversion/AIRToCpuPass.h"
 #include "air/Conversion/ConvertToAIRPass.h"
 namespace xilinx {
 namespace air {
