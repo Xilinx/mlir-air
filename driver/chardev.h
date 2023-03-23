ad07cebd0bda488d097fcc3d44a53c4d0e3f64df
Joel Nider <joel@ece.ubc.ca>
2023-03-23 15:53:10 -0700

    driver: create sysfs interface
    Use sysfs files to access the AIE address space. The address space is
    exposed over a BAR by the device but only the driver has access to it
    by default. Any user code must access the address space through the
    sysfs files; address and value.
    
    To bypass this sysfs mechanism, the driver can still be loaded with
    the parameter 'enable_aie=1'. This will enable legacy apps to mmap
    the AIE BAR directly.
    
    This requires an updated version of libxaie (xaienginev2 driver) that
    contains the sysfs backend and ability to select the backend at
    runtime.
    https://github.com/jnider/embeddedsw/tree/vck5000
    
    Signed-off-by: Joel Nider <joel@ece.ubc.ca>

--- driver/chardev.h
+++ driver/chardev.h
@@ -28,15 +28,44 @@
 #define AIE_BAR_INDEX 2
 #define BRAM_BAR_INDEX 4
 
-struct vck5000_priv {
+#define MMAP_OFFSET_AIE 0x100000ULL
+#define MMAP_OFFSET_BRAM 0x8000000ULL
+
+/*
+	This represents a single VCK5000 card.
+
+	This does not use the Linux kernel 'device' infrastructure because we want
+	to have only a single character device interface (/dev/amdair) regardless of
+	how many physical devices are attached. This follows the AMDKFD driver
+	design.
+*/
+struct vck5000_device {
+	struct kobject kobj_aie;
+
 	void __iomem *dram_bar;
+	uint64_t dram_bar_len;
+
 	void __iomem *aie_bar;
+	uint64_t aie_bar_len;
+
 	void __iomem *bram_bar;
+	uint64_t bram_bar_len;
+
 	uint64_t total_controllers;
+
+	/* device memory can be accessed indirectly through sysfs.
+		It is a two-step protocol:
+		(1) write the memory address to:
+		/sys/class/amdair/<id>/aie/address
+		(2) Read (or write) the value from:
+		/sys/class/amdair/<id>/aie/value
+	*/
+	uint64_t mem_addr; /* address for indirect memory access */
 };
 
 int vck5000_chardev_init(struct pci_dev *pdev);
 void vck5000_chardev_exit(void);
+int create_aie_mem_sysfs(struct vck5000_device *priv, uint32_t index);
 
 const char *amdair_dev_name(void);
 
