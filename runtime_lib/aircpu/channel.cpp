c456b605b5bbe94b563bb5a4c699dd10120781fa
Niansong Zhang <nz264@cornell.edu>
2023-06-09 13:52:27 -0700

    [RuntimeLib] Model channel as depth=1 FIFO

--- runtime_lib/aircpu/channel.cpp
+++ runtime_lib/aircpu/channel.cpp
@@ -2,31 +2,18 @@
 // SPDX-License-Identifier: MIT
 
 #include "air_tensor.h"
+#include "air_channel.h"
 
 #include <cstdint>
 #include <cstdio>
 
 #define VERBOSE 0
 
-/// Utility function to check if a memory address is valid
-bool isValidMemoryAddress(uint64_t address)
-{
-  try
-  {
-    volatile uint8_t *ptr = reinterpret_cast<volatile uint8_t *>(address);
-    volatile uint8_t value = *ptr; // Perform a memory access
-    return true;                   // If no exception occurs, the address is valid
-  }
-  catch (...)
-  {
-    return false; // Exception occurred, the address is invalid
-  }
-}
-
 template <typename T, int R>
 static void _air_channel_put(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *src,
                              size_t *_offset, size_t *_size, size_t *_stride)
 {
+  printf("_air_channel_put called\n");
   size_t offset[4] = {0, 0, 0, 0};
   size_t size[4] = {1, 1, 1, 1};
   size_t stride[4] = {1, 1, 1, 1};
@@ -36,8 +23,24 @@ static void _air_channel_put(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *src
     size[i] = _size[i];
     stride[i] = _stride[i];
   }
-  // allocate a buffer in heap memory
-  T *buffer = (T *)malloc(sizeof(T) * size[3] * size[2] * size[0] * size[1]);
+
+  // channel->data is an array of pointers to channel_t objects
+  // if the channel is valid, channel->data[0] should be a valid memory address
+  // otherwise, allocate a new channel
+  if (channel->data[0] == 0)
+  {
+    channel_t<T> *new_channel = (channel_t<T> *)malloc(sizeof(channel_t<T>));
+    new_channel->data = (T *)malloc(sizeof(T) * size[3] * size[2] * size[0] * size[1]);
+    new_channel->_is_full = false;
+    channel->data[0] = (uint64_t)new_channel;
+  }
+
+  channel_t<T> *chan = (channel_t<T> *)channel->data[0];
+
+  // wait until the channel is empty
+  while (chan->_is_full)
+    ;
+
   if (VERBOSE)
     printf("dst offset %lu, %lu, size %lu, %lu, stride %lu, %lu\n", offset[1],
            offset[0], size[1], size[0], stride[1], stride[0]);
@@ -50,11 +53,11 @@ static void _air_channel_put(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *src
           size_t idx =
               ((offset[3] + l) * stride[3]) + ((offset[2] + k) * stride[2]) +
               ((offset[1] + j) * stride[1]) + ((offset[0] + i) * stride[0]);
-          buffer[src_offset++] = src->data[idx];
+          chan->data[src_offset++] = src->data[idx];
         }
 
-  // save the buffer address to dst
-  channel->data[0] = (uint64_t)buffer;
+  // mark the channel as full
+  chan->_is_full = true;
 }
 
 template <typename T, int R>
@@ -62,11 +65,11 @@ static void _air_channel_get(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *dst
                              size_t *_offset, size_t *_size, size_t *_stride)
 {
   // get the buffer address from src
-  T *buffer = (T *)channel->data[0];
+  channel_t<T> *chan = (channel_t<T> *)channel->data[0];
   // test if buffer points to a valid address
-  if (!isValidMemoryAddress((uint64_t)buffer))
+  if (chan == nullptr)
   {
-    printf("channel has an invalid memory address %p\n", buffer);
+    printf("channel has an invalid memory address %p\n", chan);
     exit(1);
   }
   size_t offset[4] = {0, 0, 0, 0};
@@ -81,6 +84,11 @@ static void _air_channel_get(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *dst
   if (VERBOSE)
     printf("dst offset %lu, %lu, size %lu, %lu, stride %lu, %lu\n", offset[1],
            offset[0], size[1], size[0], stride[1], stride[0]);
+
+  // wait until the channel is full
+  while (!chan->_is_full)
+    ;
+
   // copy data from buffer to dst
   size_t dst_offset = 0;
   for (size_t l = 0; l < size[3]; l++)
@@ -91,11 +99,11 @@ static void _air_channel_get(tensor_t<uint64_t, 0> *channel, tensor_t<T, R> *dst
           size_t idx =
               ((offset[3] + l) * stride[3]) + ((offset[2] + k) * stride[2]) +
               ((offset[1] + j) * stride[1]) + ((offset[0] + i) * stride[0]);
-          dst->data[idx] = buffer[dst_offset++];
+          dst->data[idx] = chan->data[dst_offset++];
         }
 
-  // free the buffer in heap memory
-  free(buffer);
+  // mark the channel as empty
+  chan->_is_full = false;
 }
 
 template <typename T, int R>
---
+++ runtime_lib/airhost/include/air_channel.h
@@ -0,0 +1,27 @@
+//===- air_channel.h ---------------------------------------------*- C++ -*-===//
+//
+// Copyright (C) 2020-2022, Xilinx Inc.
+// Copyright (C) 2022, Advanced Micro Devices, Inc.
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef AIR_CHANNEL_H
+#define AIR_CHANNEL_H
+
+#include <stdlib.h>
+
+template <typename T>
+struct channel_t
+{
+    T *data;
+    bool _is_full;
+
+    channel_t()
+    {
+        data = nullptr;
+        _is_full = false;
+    }
+};
+
+#endif
