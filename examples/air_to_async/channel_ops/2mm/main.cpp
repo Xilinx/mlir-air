751422a351dd29ed86ddeb295af7b4a1f153e72f
Niansong Zhang <nz264@cornell.edu>
2023-07-18 16:05:08 -0700

    Lint and format

--- examples/air_to_async/channel_ops/2mm/main.cpp
+++ examples/air_to_async/channel_ops/2mm/main.cpp
@@ -20,14 +20,13 @@ template <typename T>
 void mm_out(tensor_t<T, 2> *a, tensor_t<T, 2> *b, tensor_t<T, 2> *r) {
   size_t a_h = a->shape[0];
   size_t a_w = a->shape[1];
-  size_t b_h = b->shape[0];
   size_t b_w = b->shape[1];
 
   for (size_t i = 0; i < a_h; i++) {
     for (size_t j = 0; j < b_w; j++) {
       size_t idx = i * b_w + j;
       r->data[idx] = (T)(0);
-      for (size_t k = 0, ke = a_w; k < a_w; k++) {
+      for (size_t k = 0; k < a_w; k++) {
         T _a = a->data[i * a_w + k];
         T _b = b->data[k * b_w + j];
         r->data[idx] += _a * _b;
@@ -38,7 +37,7 @@ void mm_out(tensor_t<T, 2> *a, tensor_t<T, 2> *b, tensor_t<T, 2> *r) {
 
 #define INPUT_SIZE 32
 
-int main(int argc, char *argv[]) {
+auto main(int /*argc*/, char * /*argv*/[]) -> int {
 
   tensor_t<uint32_t, 2> input_A;
   tensor_t<uint32_t, 2> input_B;
@@ -51,33 +50,33 @@ int main(int argc, char *argv[]) {
 #define M_SIZE 32
 
   input_A.shape[0] = input_A.shape[1] = M_SIZE;
-  input_A.alloc = input_A.data = (uint32_t *)malloc(
-      sizeof(uint32_t) * input_A.shape[0] * input_A.shape[1]);
+  input_A.alloc = input_A.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * input_A.shape[0] * input_A.shape[1]));
 
   input_B.shape[0] = input_B.shape[1] = M_SIZE;
-  input_B.alloc = input_B.data = (uint32_t *)malloc(
-      sizeof(uint32_t) * input_B.shape[0] * input_B.shape[1]);
+  input_B.alloc = input_B.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * input_B.shape[0] * input_B.shape[1]));
 
   input_C.shape[0] = input_C.shape[1] = M_SIZE;
-  input_C.alloc = input_C.data = (uint32_t *)malloc(
-      sizeof(uint32_t) * input_C.shape[0] * input_C.shape[1]);
+  input_C.alloc = input_C.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * input_C.shape[0] * input_C.shape[1]));
 
   output.shape[0] = output.shape[1] = M_SIZE;
-  output.alloc = output.data =
-      (uint32_t *)malloc(sizeof(uint32_t) * output.shape[0] * output.shape[1]);
+  output.alloc = output.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * output.shape[0] * output.shape[1]));
 
   output_ref0.shape[0] = output_ref0.shape[1] = M_SIZE;
-  output_ref0.alloc = output_ref0.data = (uint32_t *)malloc(
-      sizeof(uint32_t) * output_ref0.shape[0] * output_ref0.shape[1]);
+  output_ref0.alloc = output_ref0.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * output_ref0.shape[0] * output_ref0.shape[1]));
 
   output_ref1.shape[0] = output_ref1.shape[1] = M_SIZE;
-  output_ref1.alloc = output_ref1.data = (uint32_t *)malloc(
-      sizeof(uint32_t) * output_ref1.shape[0] * output_ref1.shape[1]);
+  output_ref1.alloc = output_ref1.data = static_cast<uint32_t *>(
+      malloc(sizeof(uint32_t) * output_ref1.shape[0] * output_ref1.shape[1]));
 
-  for (int i = 0; i < input_A.shape[0] * input_A.shape[1]; i++) {
-    input_A.data[i] = ((uint32_t)i) % 3;
-    input_B.data[i] = ((uint32_t)i + 1) % 5;
-    input_C.data[i] = ((uint32_t)i + 2) % 7;
+  for (unsigned i = 0; i < input_A.shape[0] * input_A.shape[1]; i++) {
+    input_A.data[i] = (static_cast<uint32_t>(i)) % 3;
+    input_B.data[i] = (static_cast<uint32_t>(i) + 1) % 5;
+    input_C.data[i] = (static_cast<uint32_t>(i) + 2) % 7;
     output.data[i] = 0;
     output_ref0.data[i] = 0;
     output_ref1.data[i] = 0;
@@ -86,27 +85,24 @@ int main(int argc, char *argv[]) {
   mm_out(&input_A, &input_B, &output_ref0);
   mm_out(&output_ref0, &input_C, &output_ref1);
 
-  void *a, *b, *c, *o;
-  a = &input_A;
-  b = &input_B;
-  c = &input_C;
-  o = &output;
-
   _mlir_ciface_forward((void *)&input_A, (void *)&input_B, (void *)&input_C,
                        (void *)&output);
 
   int errors = 0;
   auto output_size = output.shape[0] * output.shape[1];
-  for (int i = 0; i < output_size; i++) {
+  for (unsigned i = 0; i < output_size; i++) {
     auto d = output.data[i];
     auto ref = output_ref1.data[i];
     if (d != ref) {
       errors++;
-      if (errors < 10)
-        printf("%04X: mismatch %d != %d\n", i, d, ref);
+      if (errors < 10) {
+        {
+          { printf("%04X: mismatch %d != %d\n", i, d, ref); }
+        }
+      }
     }
   }
-  if (!errors) {
+  if (errors == 0) {
     printf("PASS!\n");
   } else {
     printf("fail %ld/%ld.\n", (output_size - errors), output_size);
