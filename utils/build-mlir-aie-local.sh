be5e6778ce61b433445394fc0f715602720c1a66
Erwei Wang <erweiw@xilinx.com>
2023-07-21 17:34:10 -0700

    Use up-to-date mlir-aie build flow

--- utils/build-mlir-aie-local.sh
+++ utils/build-mlir-aie-local.sh
@@ -8,35 +8,31 @@
 # 
 ##===----------------------------------------------------------------------===##
 #
-# This script build mlir-aie given the <sysroot dir>, <llvm dir> and 
-# <cmakeModules dir>. Assuming they are all in the same subfolder, it would
-# look like:
+# This script build mlir-aie given the <llvm dir>. Assuming they are all in the 
+# same subfolder, it would look like:
 #
-# build-mlir-aie-local.sh <llvm dir> <cmakeModules dir> 
-#     <mlir-aie dir> <build dir> <install dir> <libxaie dir>
+# build-mlir-aie-local.sh <llvm dir> <mlir-aie dir> <build dir> <install dir>
 #
 # e.g. build-mlir-aie-local.sh /scratch/llvm 
-#     /scratch/cmakeModules/cmakeModulesXilinx
 #
 # <mlir-aie dir> - optional, mlir-aie repo name, default is 'mlir-aie'
 # <build dir>    - optional, mlir-aie/build dir name, default is 'build'
 # <install dir>  - optional, mlir-aie/install dir name, default is 'install'
-# <libxaie dir>  - optional, libxaie installation dir, default is 
-#     '/opt/xaiengine'
 #
 ##===----------------------------------------------------------------------===##
 
-if [ "$#" -lt 2 ]; then
-    echo "ERROR: Needs at least 2 arguments for <llvm dir> and <cmakeModules dir>."
+if [ "$#" -lt 1 ]; then
+    echo "ERROR: Needs at least 1 arguments for <llvm dir>."
     exit 1
 fi
-LLVM_DIR=`realpath $1`
-CMAKEMODULES_DIR=`realpath $2`
+BASE_DIR=`realpath $(dirname $0)/..`
+CMAKEMODULES_DIR=$BASE_DIR/cmake
 
-LIBXAIE_DIR=`realpath ${3:-"/opt/xaiengine/"}`
-MLIR_AIE_DIR=${4:-"mlir-aie"}
-BUILD_DIR=${5:-"build"}
-INSTALL_DIR=${6:-"install"}
+LLVM_DIR=`realpath $1`
+
+MLIR_AIE_DIR=${2:-"mlir-aie"}
+BUILD_DIR=${3:-"build"}
+INSTALL_DIR=${4:-"install"}
 
 mkdir -p $MLIR_AIE_DIR/$BUILD_DIR
 mkdir -p $MLIR_AIE_DIR/$INSTALL_DIR
@@ -47,13 +43,13 @@ cmake -GNinja \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DLLVM_DIR=${LLVM_DIR}/build/lib/cmake/llvm \
     -DMLIR_DIR=${LLVM_DIR}/build/lib/cmake/mlir \
-    -DLibXAIE_x86_64_DIR=${LIBXAIE_DIR} \
     -DCMAKE_MODULE_PATH=${CMAKEMODULES_DIR}/ \
     -DCMAKE_INSTALL_PREFIX="../${INSTALL_DIR}" \
-    -DBUILD_SHARED_LIBS=off \
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DAIE_ENABLE_BINDINGS_PYTHON=ON \
+    "-DAIE_RUNTIME_TARGETS=x86_64;aarch64" \
+    -DAIE_RUNTIME_TEST_TARGET=aarch64 \
     .. |& tee cmake.log
 
 ninja |& tee ninja.log
