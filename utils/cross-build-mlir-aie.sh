ee0c6fb8f1537de1f1d3292fd2f11265e540d5af
Erwei Wang <erweiw@xilinx.com>
2023-07-25 10:27:34 -0700

    Use your own toolchain file

--- utils/cross-build-mlir-aie.sh
+++ utils/cross-build-mlir-aie.sh
@@ -10,9 +10,10 @@
 # This script build mlir-aie given the <sysroot dir> and <llvm dir>. Assuming 
 # they are all in the same subfolder, it would look like:
 #
-# cross-build-mlir-aie.sh <sysroot dir> <llvm dir> <install dir> <mlir-aie dir> 
-#     <build dir>
+# cross-build-mlir-aie.sh <toolchain fine> <sysroot dir> <llvm dir> 
+#     <install dir> <mlir-aie dir> <build dir>
 #
+# <toolchain file> - absolute path to cmake toolchain file
 # <sysroot dir> - sysroot, absolute directory
 # <llvm dir>     - llvm location, absolute directory
 # <install dir>  - optional, default is 'install-aarch64'
@@ -21,17 +22,18 @@
 #
 ##===----------------------------------------------------------------------===##
 
-if [ "$#" -lt 2 ]; then
-    echo "ERROR: Needs at least 2 arguments for <sysroot dir> and <llvm dir>."
+if [ "$#" -lt 3 ]; then
+    echo "ERROR: Needs at least 3 arguments for <toolchain file>, <sysroot dir> and <llvm dir>."
     exit 1
 fi
 
-CMAKE_SYSROOT=`realpath $1`
-LLVM_DIR=`realpath $2`
+CMAKE_TOOLCHAIN_FILE=`realpath $1`
+CMAKE_SYSROOT=`realpath $2`
+LLVM_DIR=`realpath $3`
 
-INSTALL_DIR=${3:-"install-aarch64"}
-MLIR_AIE_DIR=${4:-"mlir-aie"}
-BUILD_DIR=${5:-"${MLIR_AIE_DIR}/build-aarch64"}
+INSTALL_DIR=${4:-"install-aarch64"}
+MLIR_AIE_DIR=${5:-"mlir-aie"}
+BUILD_DIR=${6:-"${MLIR_AIE_DIR}/build-aarch64"}
 
 CMAKEMODULES_DIR=`realpath $MLIR_AIE_DIR/cmake`
 
@@ -46,18 +48,29 @@ set -o pipefail
 set -e
 cmake -GNinja \
     -DCMAKE_MODULE_PATH=${CMAKEMODULES_DIR}/modulesXilinx \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKEMODULES_DIR}/toolchainFiles/toolchain_clang_crosscomp_pynq.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
     -DSysroot=${CMAKE_SYSROOT} \
     -DArch=arm64 \
     -DLLVM_DIR=${LLVM_DIR}/build-aarch64/lib/cmake/llvm \
     -DMLIR_DIR=${LLVM_DIR}/build-aarch64/lib/cmake/mlir \
-    -DLLVM_USE_LINKER=lld \
     -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
     -DAIE_ENABLE_BINDINGS_PYTHON=ON \
     -DCMAKE_BUILD_TYPE=Debug \
     -Wno-dev \
     .. |& tee cmake.log
 
+ec=$?
+if [ $ec -ne 0 ]; then
+    echo "CMake Error"
+    exit $ec
+fi
+
 ninja install |& tee ninja-install.log
+
+ec=$?
+if [ $ec -ne 0 ]; then
+    echo "Build Error"
+    exit $ec
+fi
 
 exit 0
