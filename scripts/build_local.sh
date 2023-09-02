ae87341a4bd5b887732190c5b633523a79c0a8f8
max <maksim.levental@gmail.com>
2023-09-02 12:19:44 -0500

    python bindings wheels working locally

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -13,10 +13,8 @@ case "${unameOut}" in
 esac
 echo "${machine}"
 
-export APPLY_PATCHES=true
-#export PIP_FIND_LINKS="https://makslevental.github.io/wheels"
-export MLIR_WHEEL_VERSION="17.0.0.2023083019+35ca6498"
-export MLIR_AIE_WHEEL_VERSION="0.0.1.2023090122+c7add68"
+export MLIR_WHEEL_VERSION="18.0.0.2023091015+780b046b"
+export MLIR_AIE_WHEEL_VERSION="0.0.1.2023091105+a0cc3d0"
 
 if [ "$machine" == "linux" ]; then
   export CIBW_ARCHS=${CIBW_ARCHS:-x86_64}
@@ -41,10 +39,15 @@ if [ x"$CIBW_ARCHS" == x"aarch64" ]; then
   export PIP_NO_BUILD_ISOLATION="false"
   pip install -r $HERE/../requirements.txt
   $HERE/../scripts/pip_install_mlir.sh
-  $HERE/../scripts/apply_patches.sh
 
   CMAKE_GENERATOR=Ninja \
   pip wheel $HERE/.. -v -w $HERE/../wheelhouse
 else
   cibuildwheel "$HERE"/.. --platform "$machine"
 fi
+
+cp -R "$HERE/../scripts" "$HERE/../python_bindings"
+
+pushd "$HERE/../python_bindings"
+
+cibuildwheel --platform "$machine" --output-dir ../wheelhouse
--- scripts/pip_install_mlir_air.sh
+++ scripts/pip_install_mlir_air.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -xe
 
-export PIP_FIND_LINKS="https://github.com/makslevental/mlir-air/releases/expanded_assets/latest"
-
 SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
 
 if [ ! -z "$MLIR_AIR_WHEEL_VERSION" ]; then
@@ -15,7 +13,17 @@ if [ x"$CIBW_ARCHS" == x"arm64" ] || [ x"$CIBW_ARCHS" == x"aarch64" ]; then
   elif [ x"$MATRIX_OS" == x"ubuntu-20.04" ] && [ x"$CIBW_ARCHS" == x"aarch64" ]; then
     PLAT=linux_aarch64
   fi
-  pip install mlir-air$MLIR_AIR_WHEEL_VERSION --platform $PLAT --only-binary=:all: --target $SITE_PACKAGES  --no-deps --force -U
+
+  pip install mlir-air$MLIR_AIR_WHEEL_VERSION --platform $PLAT --only-binary=:all: --target $SITE_PACKAGES  --no-deps --force -U \
+    -f https://makslevental.github.io/wheels \
+    -f https://github.com/Xilinx/mlir-aie/releases/expanded_assets/latest-wheels \
+    -f https://github.com/Xilinx/mlir-air/releases/expanded_assets/latest-wheels
+
 else
-  pip install mlir-air$MLIR_AIR_WHEEL_VERSION --force -U
+
+  pip install mlir-air$MLIR_AIR_WHEEL_VERSION --force -U \
+    -f https://makslevental.github.io/wheels \
+    -f https://github.com/Xilinx/mlir-aie/releases/expanded_assets/latest-wheels \
+    -f https://github.com/Xilinx/mlir-air/releases/expanded_assets/latest-wheels
+
 fi
