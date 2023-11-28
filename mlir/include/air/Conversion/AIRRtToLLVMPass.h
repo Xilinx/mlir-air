0aa0b29f14843d3b02366c7904b8696219affbaf
Abhishek Varma <avarma094@gmail.com>
2023-11-28 05:14:07 +0000

    Deal with airrt

--- mlir/include/air/Conversion/AIRRtToLLVMPass.h
+++ mlir/include/air/Conversion/AIRRtToLLVMPass.h
@@ -9,7 +9,7 @@
 #ifndef AIRRT_TO_LLVM_H
 #define AIRRT_TO_LLVM_H
 
-#include "air/Conversion/PassDetail.h"
+// #include "air/Conversion/PassDetail.h"
 
 #include "mlir/Pass/Pass.h"
 #include <memory>
@@ -17,6 +17,9 @@
 namespace xilinx {
 namespace airrt {
 
+using namespace mlir;
+#define GEN_PASS_DEF_AIRRTTOLLVM
+#include "air/Conversion/Passes.h.inc"
 std::unique_ptr<mlir::Pass> createAIRRtToLLVMPass();
 
 } // namespace airrt
