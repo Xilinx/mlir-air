12633ee1e648d6418205f73a33fd14d2cf43f73c
James Newling <james.newling@gmail.com>
2024-04-24 16:59:36 -0700

    format

--- mlir/lib/Conversion/AIRRtToNpuPass.cpp
+++ mlir/lib/Conversion/AIRRtToNpuPass.cpp
@@ -1163,21 +1163,21 @@ struct AIRRtToNpuPass : public impl::AIRRtToNpuBase<AIRRtToNpuPass> {
       f.walk([&](AIEX::NpuDmaMemcpyNdOp dma) { dmas.push_back(dma); });
       auto d = f->getParentOfType<AIE::DeviceOp>();
 
-     // Performance optimization: instead of repeating calls to
-     // getAllocOpForSymbol with the same symbol name, cache the result of the
-     // first call and use the cache for subsequent calls. This dramatically
-     // improves compile time for some designs.
-     llvm::DenseMap<StringRef, std::optional<AIE::ShimDMAAllocationOp>>
-         allocationCache;
-     auto getAllocOpForSymbolWithCaching = [&](StringRef sym_name) {
-       auto iter = allocationCache.find(sym_name);
-       if (iter != allocationCache.end()) {
-         return iter->second;
-       }
-       auto infaOp = getAllocOpForSymbol(d, sym_name);
-       allocationCache.insert({sym_name, infaOp});
-       return infaOp;
-     };
+      // Performance optimization: instead of repeating calls to
+      // getAllocOpForSymbol with the same symbol name, cache the result of the
+      // first call and use the cache for subsequent calls. This dramatically
+      // improves compile time for some designs.
+      llvm::DenseMap<StringRef, std::optional<AIE::ShimDMAAllocationOp>>
+          allocationCache;
+      auto getAllocOpForSymbolWithCaching = [&](StringRef sym_name) {
+        auto iter = allocationCache.find(sym_name);
+        if (iter != allocationCache.end()) {
+          return iter->second;
+        }
+        auto infaOp = getAllocOpForSymbol(d, sym_name);
+        allocationCache.insert({sym_name, infaOp});
+        return infaOp;
+      };
 
       if (!d)
         return;
@@ -1227,21 +1227,21 @@ struct AIRRtToNpuPass : public impl::AIRRtToNpuBase<AIRRtToNpuPass> {
     AIE::DeviceOp d = nullptr;
     blk->walk([&](AIE::DeviceOp op) { d = op; });
 
-     // Performance optimization: instead of repeating calls to
-     // getAllocOpForSymbol with the same symbol name, cache the result of the
-     // first call and use the cache for subsequent calls. This dramatically
-     // improves compile time for some designs.
-     llvm::DenseMap<StringRef, std::optional<AIE::ShimDMAAllocationOp>>
-         allocationCache;
-     auto getAllocOpForSymbolWithCaching = [&](StringRef sym_name) {
-       auto iter = allocationCache.find(sym_name);
-       if (iter != allocationCache.end()) {
-         return iter->second;
-       }
-       auto infaOp = getAllocOpForSymbol(d, sym_name);
-       allocationCache.insert({sym_name, infaOp});
-       return infaOp;
-     };
+    // Performance optimization: instead of repeating calls to
+    // getAllocOpForSymbol with the same symbol name, cache the result of the
+    // first call and use the cache for subsequent calls. This dramatically
+    // improves compile time for some designs.
+    llvm::DenseMap<StringRef, std::optional<AIE::ShimDMAAllocationOp>>
+        allocationCache;
+    auto getAllocOpForSymbolWithCaching = [&](StringRef sym_name) {
+      auto iter = allocationCache.find(sym_name);
+      if (iter != allocationCache.end()) {
+        return iter->second;
+      }
+      auto infaOp = getAllocOpForSymbol(d, sym_name);
+      allocationCache.insert({sym_name, infaOp});
+      return infaOp;
+    };
 
     blk->walk([&](Operation *op) {
       if (auto dma = dyn_cast<AIEX::NpuDmaMemcpyNdOp>(op)) {
