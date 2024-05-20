060823162cd7d7672153036f4bf218de969a4de2
Jeff Fifield <jeff.fifield@amd.com>
2024-05-20 08:50:06 -0600

    emit-while-loop=true

--- python/air/compiler/aircc/main.py
+++ python/air/compiler/aircc/main.py
@@ -373,7 +373,7 @@ def run(mlir_module, args=None):
             "builtin.module(" + pass_pipeline + ")", air_placed_module, opts, air_placed
         )
 
-        air_to_aie_pass = "air-to-aie{emit-while-loop=false"
+        air_to_aie_pass = "air-to-aie{emit-while-loop=true"
         air_to_aie_pass = air_to_aie_pass + f" row-offset={opts.row_offset}"
         air_to_aie_pass = air_to_aie_pass + f" col-offset={opts.col_offset}"
         air_to_aie_pass = air_to_aie_pass + f" device={opts.device}" + "}"
