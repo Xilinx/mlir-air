db254882b0c21d7c766d90305e826dc5cc2f4031
Erika Hunhoff <erika.hunhoff@amd.com>
2024-07-02 14:03:35 -0600

    Add channel size example

---
+++ programming_examples/channel_examples/channel_size/herd_to_herd.py
@@ -0,0 +1,137 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+import sys
+from pathlib import Path  # if you haven't already done so
+
+# Python paths are a bit complex. Taking solution from : https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError:  # Already removed
+    pass
+
+from air.ir import *
+from air.dialects.air import *
+from air.dialects.linalg import elemwise_binary
+from air.dialects.linalg.opdsl.lang import BinaryFn, TypeFn
+from air.dialects.memref import AllocOp, DeallocOp, load, store
+from air.dialects.func import FuncOp
+from air.dialects.scf import for_, yield_
+
+range_ = for_
+
+
+from common import *
+
+CHANNEL_SIZE = 4
+assert IMAGE_SIZE[0] % CHANNEL_SIZE == 0
+
+
+@module_builder
+def build_module():
+    memrefTyInOut = MemRefType.get(IMAGE_SIZE, T.i32())
+
+    # We want to store our data in L1 memory
+    mem_space_l1 = IntegerAttr.get(T.i32(), MemorySpace.L1)
+
+    # This is the type definition of the tile
+    partial_image_type_l1 = MemRefType.get(
+        shape=[
+            IMAGE_SIZE[0] // CHANNEL_SIZE,
+            IMAGE_SIZE[1],
+        ],  # Process half the image at a time
+        element_type=T.i32(),
+        memory_space=mem_space_l1,
+    )
+
+    # Create two channels which will send/receive the
+    # input/output data respectively
+    ChannelOp("ChanIn")
+    ChannelOp("ChanOut")
+
+    # Create a channel we will use to pass data between works in two herds
+    ChannelOp("Herd2Herd", size=[1, CHANNEL_SIZE])
+
+    # We will send an image worth of data in and out
+    @FuncOp.from_py_func(memrefTyInOut, memrefTyInOut)
+    def copy(arg0, arg1):
+
+        # The arguments are the input and output
+        @launch(operands=[arg0, arg1])
+        def launch_body(a, b):
+
+            # Fetch all input data into the channel
+            ChannelPut("ChanIn", a)
+
+            # Push all output data out of the channel
+            ChannelGet("ChanOut", b)
+
+            @segment(name="seg")
+            def segment_body():
+
+                @herd(name="producer_herd", sizes=[1, 1])
+                def herd_body(tx, ty, sx, sy):
+
+                    for channel_index in range(CHANNEL_SIZE):
+                        # We must allocate a buffer of tile size for the input/output
+                        partial_image_in = AllocOp(partial_image_type_l1, [], [])
+                        partial_image_out = AllocOp(partial_image_type_l1, [], [])
+
+                        ChannelGet("ChanIn", partial_image_in)
+
+                        elemwise_binary(
+                            partial_image_in,
+                            partial_image_in,
+                            outs=[partial_image_out],
+                            fun=BinaryFn.mul,
+                            cast=TypeFn.cast_unsigned,
+                        )
+
+                        ChannelPut(
+                            "Herd2Herd", partial_image_out, indices=[1, channel_index]
+                        )
+
+                        DeallocOp(partial_image_in)
+                        DeallocOp(partial_image_out)
+
+                @herd(name="consumer_herd", sizes=[1, 1])
+                def herd_body(tx, ty, sx, sy):
+
+                    for channel_index in range(CHANNEL_SIZE):
+                        # We must allocate a buffer of image size for the input/output
+                        partial_image_in = AllocOp(partial_image_type_l1, [], [])
+                        partial_image_out = AllocOp(partial_image_type_l1, [], [])
+
+                        ChannelGet(
+                            "Herd2Herd", partial_image_in, indices=[1, channel_index]
+                        )
+
+                        # Access every value in the image
+                        for j in range_(IMAGE_HEIGHT):
+                            for i in range_(IMAGE_WIDTH):
+                                # Load the input value
+                                val_in = load(partial_image_in, [i, j])
+
+                                # Calculate the output value
+                                val_out = arith.addi(
+                                    val_in, arith.ConstantOp(T.i32(), 1)
+                                )
+
+                                # Store the output value
+                                store(val_out, partial_image_out, [i, j])
+                                yield_([])
+                            yield_([])
+
+                        ChannelPut("ChanOut", partial_image_out)
+
+                        DeallocOp(partial_image_in)
+                        DeallocOp(partial_image_out)
+
+
+if __name__ == "__main__":
+    module = build_module()
+    print(module)
