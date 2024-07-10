ec12e5ede2dd35ad1e71aadb07361abfec3e19a8
erweiw <erwei.wang@amd.com>
2024-07-09 18:32:57 -0600

    Remove libxaie path

--- utils/build-mlir-aie-local.sh
+++ utils/build-mlir-aie-local.sh
@@ -13,7 +13,7 @@
 # look like:
 #
 # build-mlir-aie-local.sh <llvm dir> <cmakeModules dir> 
-#     <mlir-aie dir> <build dir> <install dir> <libxaie dir>
+#     <mlir-aie dir> <build dir> <install dir>
 #
 # e.g. build-mlir-aie-local.sh /scratch/llvm 
 #     /scratch/cmakeModules/cmakeModulesXilinx
@@ -21,8 +21,6 @@
 # <mlir-aie dir> - optional, mlir-aie repo name, default is 'mlir-aie'
 # <build dir>    - optional, mlir-aie/build dir name, default is 'build'
 # <install dir>  - optional, mlir-aie/install dir name, default is 'install'
-# <libxaie dir>  - optional, libxaie installation dir, default is 
-#     '/opt/xaiengine'
 #
 ##===----------------------------------------------------------------------===##
 
@@ -33,10 +31,9 @@ fi
 LLVM_DIR=`realpath $1`
 CMAKEMODULES_DIR=`realpath $2`
 
-LIBXAIE_DIR=`realpath ${3:-"/opt/xaiengine/"}`
-MLIR_AIE_DIR=${4:-"mlir-aie"}
-BUILD_DIR=${5:-"build"}
-INSTALL_DIR=${6:-"install"}
+MLIR_AIE_DIR=${3:-"mlir-aie"}
+BUILD_DIR=${4:-"build"}
+INSTALL_DIR=${5:-"install"}
 
 mkdir -p $MLIR_AIE_DIR/$BUILD_DIR
 mkdir -p $MLIR_AIE_DIR/$INSTALL_DIR
@@ -47,7 +44,6 @@ cmake -GNinja \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DLLVM_DIR=${LLVM_DIR}/build/lib/cmake/llvm \
     -DMLIR_DIR=${LLVM_DIR}/build/lib/cmake/mlir \
-    -DLibXAIE_x86_64_DIR=${LIBXAIE_DIR} \
     -DCMAKE_MODULE_PATH=${CMAKEMODULES_DIR}/ \
     -DCMAKE_INSTALL_PREFIX="../${INSTALL_DIR}" \
     -DBUILD_SHARED_LIBS=off \
