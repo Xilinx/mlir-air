70eb71e8185f2cd59d4058007f9aa168e76181d7
Erika Hunhoff <erika.hunhoff@amd.com>
2024-07-12 13:24:42 -0600

    Add logic to try to access l2 tile in herd

--- programming_examples/segment_alloc/segment_alloc.py
+++ programming_examples/segment_alloc/segment_alloc.py
@@ -33,30 +33,50 @@ def build_module():
             # The arguments are still the input and the output
             @segment(name="seg", operands=[a, b])
             def segment_body(arg2, arg3):
+                # We want to store our data in L1 memory
+                mem_space_l2 = IntegerAttr.get(T.i32(), MemorySpace.L2)
+
+                # This is the type definition of the tile
+                tile_type_l2 = MemRefType.get(
+                    shape=TILE_SIZE,
+                    element_type=T.i32(),
+                    memory_space=mem_space_l2,
+                )
+
+                # We must allocate a buffer of tile size for the input/output
+                tile_in_l2 = AllocOp(tile_type_l2, [], [])
 
                 # The herd sizes correspond to the dimensions of the contiguous block of cores we are hoping to get.
                 # We just need one compute core, so we ask for a 1x1 herd
-                @herd(name="copyherd", sizes=[1, 1], operands=[arg2, arg3])
-                def herd_body(tx, ty, sx, sy, a, b):
+                @herd(name="copyherd", sizes=[1, 1], operands=[arg2, arg3, tile_in_l2])
+                def herd_body(tx, ty, sx, sy, a, b, my_l2_tile):
 
                     # We want to store our data in L1 memory
-                    mem_space = IntegerAttr.get(T.i32(), MemorySpace.L1)
+                    mem_space_l1 = IntegerAttr.get(T.i32(), MemorySpace.L1)
 
                     # This is the type definition of the tile
-                    tile_type = MemRefType.get(
+                    tile_type_l1 = MemRefType.get(
                         shape=TILE_SIZE,
                         element_type=T.i32(),
-                        memory_space=mem_space,
+                        memory_space=mem_space_l1,
                     )
 
                     # We must allocate a buffer of tile size for the input/output
-                    tile_in = AllocOp(tile_type, [], [])
-                    tile_out = AllocOp(tile_type, [], [])
+                    tile_in_l1 = AllocOp(tile_type_l1, [], [])
+                    tile_out_l1 = AllocOp(tile_type_l1, [], [])
+
+                    dma_memcpy_nd(
+                        my_l2_tile,
+                        a,
+                        src_offsets=[0, 0],
+                        src_sizes=[TILE_HEIGHT, TILE_WIDTH],
+                        src_strides=[IMAGE_WIDTH, 1],
+                    )
 
                     # Copy a tile from the input image (a) into the L1 memory region (tile_in)
                     dma_memcpy_nd(
-                        tile_in,
-                        a,
+                        tile_in_l1,
+                        my_l2_tile,
                         src_offsets=[0, 0],
                         src_sizes=[TILE_HEIGHT, TILE_WIDTH],
                         src_strides=[IMAGE_WIDTH, 1],
@@ -66,25 +86,25 @@ def build_module():
                     for j in range_(TILE_HEIGHT):
                         for i in range_(TILE_WIDTH):
                             # Load the input value from tile_in
-                            val = load(tile_in, [i, j])
+                            val = load(tile_in_l1, [i, j])
 
                             # Store the output value in tile_out
-                            store(val, tile_out, [i, j])
+                            store(val, tile_out_l1, [i, j])
                             yield_([])
                         yield_([])
 
                     # Copy the output tile into the output
                     dma_memcpy_nd(
                         b,
-                        tile_out,
+                        tile_out_l1,
                         dst_offsets=[0, 0],
                         dst_sizes=[TILE_HEIGHT, TILE_WIDTH],
                         dst_strides=[IMAGE_WIDTH, 1],
                     )
 
                     # Deallocate our L1 buffers
-                    DeallocOp(tile_in)
-                    DeallocOp(tile_out)
+                    DeallocOp(tile_in_l1)
+                    DeallocOp(tile_out_l1)
 
 
 if __name__ == "__main__":
