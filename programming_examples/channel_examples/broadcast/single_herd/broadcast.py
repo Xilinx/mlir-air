d02fe8b29d50cea4bf159c5219ad206d3eb72614
Erika Hunhoff <erika.hunhoff@amd.com>
2024-07-30 11:05:18 -0600

    Stub out format of broadcast example

---
+++ programming_examples/channel_examples/broadcast/single_herd/broadcast.py
@@ -0,0 +1,81 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: MIT
+
+from air.ir import *
+from air.dialects.air import *
+from air.dialects.memref import AllocOp, DeallocOp, load, store
+from air.dialects.func import FuncOp
+from air.dialects.scf import for_, yield_
+from air.dialects.affine import apply as affine_apply
+
+range_ = for_
+
+IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 16
+IMAGE_SIZE = [IMAGE_HEIGHT, IMAGE_WIDTH]
+
+INOUT_DATATYPE = np.int32
+
+
+@module_builder
+def build_module():
+    xrt_dtype = T.i32()
+    memrefTyInOut = MemRefType.get(IMAGE_SIZE, xrt_dtype)
+
+    mem_space_l1 = IntegerAttr.get(T.i32(), MemorySpace.L1)
+    image_type_l1 = MemRefType.get(
+        shape=IMAGE_SIZE,
+        element_type=xrt_dtype,
+        memory_space=mem_space_l1,
+    )
+
+    ChannelOp("ChanIn")
+    ChannelOp("ChanOut")
+
+    # We will send an image worth of data in and out
+    @FuncOp.from_py_func(memrefTyInOut, memrefTyInOut, memrefTyInOut, memrefTyInOut)
+    def copy(arg0, arg1, arg2, arg3):
+
+        # The arguments are the input and output
+        @launch(operands=[arg0, arg1, arg2, arg3])
+        def launch_body(a, b, c, d):
+
+            ChannelPut("ChanIn", a)
+            ChannelGet("ChanOut", b)
+
+            @segment(name="seg")
+            def segment_body():
+
+                @herd(name="addherd", sizes=[1, 1])
+                def herd_body(_tx, _ty, _sx, _sy):
+
+                    # We must allocate a buffer of image size for the input/output
+                    image_in = AllocOp(image_type_l1, [], [])
+                    image_out = AllocOp(image_type_l1, [], [])
+
+                    ChannelGet("ChanIn", image_in)
+
+                    # Access every value in the image
+                    for i in range_(IMAGE_HEIGHT):
+                        for j in range_(IMAGE_WIDTH):
+                            # Load the input value
+                            val_in = load(image_in, [i, j])
+
+                            # Calculate the output value
+                            val_out = arith.addi(val_in, arith.ConstantOp(xrt_dtype, 1))
+
+                            # Store the output value
+                            store(val_out, image_out, [i, j])
+                            yield_([])
+                        yield_([])
+
+                    ChannelPut("ChanOut", image_out)
+
+                    DeallocOp(image_in)
+                    DeallocOp(image_out)
+
+
+
+if __name__ == "__main__":
+    module = build_module()
+    print(module)
---
+++ programming_examples/channel_examples/broadcast/single_herd/run.py
@@ -0,0 +1,85 @@
+# run.py -*- Python -*-
+#
+# Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+import argparse
+import numpy as np
+import air.backend.xrt as xrt_backend
+import filelock
+
+from broadcast import *
+
+INOUT_DATATYPE = np.uint32
+INOUT_ELEM_SIZE = np.dtype(INOUT_DATATYPE).itemsize
+INOUT_SIZE = IMAGE_SIZE[0] * IMAGE_SIZE[1]
+INOUT_SIZE_BYTES = INOUT_SIZE * INOUT_ELEM_SIZE
+
+
+def print_matrix(matrix_array):
+    for i in range(IMAGE_HEIGHT):
+        row = matrix_array[i * IMAGE_WIDTH : (i + 1) * IMAGE_WIDTH]
+        for val in row:
+            val = val & 0xFFFF
+            print(f"{val:04x}", end=" ")
+        print("")
+
+
+def test_main(build_module, verbose=False):
+    mlir_module = build_module()
+
+    input_a = np.arange(1, INOUT_SIZE + 1, dtype=INOUT_DATATYPE)
+    input_b = np.arange(1, INOUT_SIZE + 1, dtype=INOUT_DATATYPE)
+    input_c = np.arange(1, INOUT_SIZE + 1, dtype=INOUT_DATATYPE)
+    input_d = np.arange(1, INOUT_SIZE + 1, dtype=INOUT_DATATYPE)
+
+    backend = xrt_backend.XRTBackend(
+        verbose=verbose, experimental_passes=True, omit_while_true_loop=True
+    )
+
+    if verbose:
+        print_matrix(input_b)
+
+    # run the module
+    with filelock.FileLock("/tmp/npu.lock"):
+        broadcast = backend.compile_and_load(mlir_module)
+        (_, output_b, output_c, output_d) = broadcast(input_a, input_b, input_c, input_d)
+
+    backend.unload()
+
+    if verbose:
+        print_matrix(output_b)
+
+    # check output, should have all values incremented
+    errors = 0
+    outputs = [output_b, output_c, output_d]
+    for k in range(3):
+        current_output = outputs[k]
+        for i in range(INOUT_SIZE):
+            rb = current_output[i]
+            expected_value = input_a[i] + (k + 1)
+
+            # value should have been updated
+            if not (rb == expected_value):
+                errors += 1
+
+    if errors == 0:
+        print("PASS!")
+        exit(0)
+    else:
+        print("failed. errors=", errors)
+        exit(-1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="run.py",
+        description="Builds, runs, and tests the channel_examples/broadcast/single_herd example",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    test_main(build_module, verbose=args.verbose)
