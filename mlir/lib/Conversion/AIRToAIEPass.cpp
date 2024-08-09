f794a045307d80b447db7d10c5080f5a5da52dcc
Akash K <134331715+akkothar@users.noreply.github.com>
2024-08-08 23:00:59 -0500

    Fix code  format

--- mlir/lib/Conversion/AIRToAIEPass.cpp
+++ mlir/lib/Conversion/AIRToAIEPass.cpp
@@ -535,8 +535,9 @@ void createAIEModulesAndOutlineCores(
                      StringAttr::get(builder.getContext(), segment_name));
 
     aie_dev.getRegion().emplaceBlock();
-    seg.walk(
-        [&](xilinx::air::HerdOp h) { aie_modules.push_back({aie_dev, h}); });
+    seg.walk([&](xilinx::air::HerdOp h) {
+      aie_modules.push_back({aie_dev, h});
+    });
     // If the device has memtiles, then outline memtiles
     if (aie_dev.getTargetModel().getNumMemTileRows()) {
       outlineAIEMemtiles(builder, aie_dev, seg, options);
@@ -3573,8 +3574,9 @@ FailureOr<ModuleOp> convertAIRToAIE(mlir::RewriterBase &rewriter,
                                        /*.trace_size = */ 0,
                                        /* .device = */ *device};
   std::vector<std::pair<ModuleOp, xilinx::air::HerdOp>> aie_modules;
-  p.walk(
-      [&](xilinx::air::HerdOp h) { aie_modules.push_back({aie_module, h}); });
+  p.walk([&](xilinx::air::HerdOp h) {
+    aie_modules.push_back({aie_module, h});
+  });
   std::map<AIE::TileOp, air::HerdOp> tileToHerdMap;
   for (auto &p : aie_modules) {
     ModuleOp aie_module = std::get<0>(p);
