0eb23fc55aeaed4a52fa65c7fbd257fd77d6884e
Erika Hunhoff <erika.hunhoff@amd.com>
2024-08-13 13:47:42 -0600

    closer to working

--- programming_examples/channel_examples/broadcast/single_herd/broadcast.py
+++ programming_examples/channel_examples/broadcast/single_herd/broadcast.py
@@ -31,8 +31,8 @@ def build_module():
         memory_space=mem_space_l1,
     )
 
-    ChannelOp("ChanIn")
-    ChannelOp("ChanOut", size=[1, 3])
+    Channel("ChanIn", size=[1, 1], broadcast_shape=[1, 3])
+    Channel("ChanOut", size=[1, 3])
 
     # We will send an image worth of data in and out
     @FuncOp.from_py_func(memrefTyInOut, memrefTyInOut, memrefTyInOut, memrefTyInOut)
@@ -57,7 +57,7 @@ def build_module():
                     image_in = AllocOp(image_type_l1, [], [])
                     image_out = AllocOp(image_type_l1, [], [])
 
-                    ChannelGet("ChanIn", image_in)
+                    ChannelGet("ChanIn", image_in, indices=[tx, ty])
 
                     # Access every value in the image
                     for i in range_(IMAGE_HEIGHT):
