4116cf5148b268e093caee0d9f7c2fd50ffd5876
erwei-xilinx <erwei.wang@amd.com>
2024-11-13 16:18:01 -0800

    Remove assertions in air.execute verifier

--- mlir/lib/Dialect/AIR/IR/AIRDialect.cpp
+++ mlir/lib/Dialect/AIR/IR/AIRDialect.cpp
@@ -1026,9 +1026,12 @@ uint64_t HerdOp::getNumRows() {
 //
 
 LogicalResult ExecuteOp::verify() {
-  assert(getOperation()->getNumRegions() == 1 && "ExecuteOp has zero region!");
-  assert(!getRegion().empty() && "ExecuteOp should have non-empty region");
-  assert(!getBody().empty() && "ExecuteOp should have non-empty body");
+  if (getOperation()->getNumRegions() != 1)
+    return emitOpError("ExecuteOp has zero region.");
+  if (getRegion().empty())
+    return emitOpError("ExecuteOp should have non-empty region.");
+  if (getBody().empty())
+    return emitOpError("ExecuteOp should have non-empty body.");
 
   return success();
 }
