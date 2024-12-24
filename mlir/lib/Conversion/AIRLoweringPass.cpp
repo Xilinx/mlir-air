d2558039d8c328df4a7e8c70e13c080e707deb41
erwei-xilinx <erwei.wang@amd.com>
2024-12-23 18:42:09 -0800

    Fixup assumption that air.launch must exist

--- mlir/lib/Conversion/AIRLoweringPass.cpp
+++ mlir/lib/Conversion/AIRLoweringPass.cpp
@@ -929,16 +929,18 @@ public:
 
     // Preprocess the IR's L3 dma bds by applying loop splitting, fusion and
     // specialization patterns.
-    SmallVector<Region *> launchRegs;
-    module.walk([&launchRegs](air::LaunchOp launch) {
-      launchRegs.push_back(&launch.getRegion());
+    SmallVector<Region *> regions;
+    module.walk([&regions](func::FuncOp func) {
+      regions.push_back(&func.getRegion());
     });
-    for (auto region : launchRegs) {
+    for (auto region : regions) {
       // Split async for loops into asynchronously independent loop nests.
       air::applyAIRIsolateAsyncDmaLoopNestsPattern(region);
       air::applyAIRSpecializeChannelWrapAndStridePattern(region,
                                                          /*maxNumDims*/ 4);
     }
+    // TODO: Disable loop unrolling in patterns above, and add AIRLoopFusion
+    // pattern here to handle any potential out-of-bd scenarios.
 
     TypeConverter converter;
     converter.addConversion([&](Type type) -> std::optional<Type> {
