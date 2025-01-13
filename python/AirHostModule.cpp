929eca4d85cde6065c997a13fbf06e97f64c6461
erwei-xilinx <erwei.wang@amd.com>
2025-01-13 00:39:36 -0800

    Switch air python bindings from pybind11 to nanobind

--- python/AirHostModule.cpp
+++ python/AirHostModule.cpp
@@ -7,9 +7,9 @@
 //===----------------------------------------------------------------------===//
 
 #include <iostream>
-#include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/stl.h>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
 
 #include "air.hpp"
 #include "hsa/hsa.h"
@@ -17,14 +17,14 @@
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-namespace py = pybind11;
+namespace nb = nanobind;
 
 namespace {
-void defineAIRHostModule(pybind11::module &m) {
+void defineAIRHostModule(nanobind::module &m) {
 
   m.def(
       "init_libxaie", []() -> uint64_t { return (uint64_t)air_init_libxaie(); },
-      pybind11::return_value_policy::reference);
+      nanobind::return_value_policy::reference);
 
   m.def("deinit_libxaie", [](uint64_t ctx) -> void {
     air_deinit_libxaie((air_libxaie_ctx_t)ctx);
@@ -34,7 +34,7 @@ void defineAIRHostModule(pybind11::module &m) {
 
   m.def("shut_down", []() -> uint64_t { return (uint64_t)air_shut_down(); });
 
-  pybind11::class_<air_module_desc_t>(m, "ModuleDescriptor")
+  nanobind::class_<air_module_desc_t>(m, "ModuleDescriptor")
       .def(
           "getSegments",
           [](const air_module_desc_t &d) -> std::vector<air_segment_desc_t *> {
@@ -43,9 +43,10 @@ void defineAIRHostModule(pybind11::module &m) {
               segments.push_back(d.segment_descs[i]);
             return segments;
           },
-          pybind11::return_value_policy::reference);
+          nanobind::return_value_policy::reference);
+  // pybind11::return_value_policy::reference);
 
-  pybind11::class_<air_segment_desc_t>(m, "SegmentDescriptor")
+  nanobind::class_<air_segment_desc_t>(m, "SegmentDescriptor")
       .def(
           "getHerds",
           [](const air_segment_desc_t &d) -> std::vector<air_herd_desc_t *> {
@@ -54,12 +55,12 @@ void defineAIRHostModule(pybind11::module &m) {
               herds.push_back(d.herd_descs[i]);
             return herds;
           },
-          pybind11::return_value_policy::reference)
+          nanobind::return_value_policy::reference)
       .def("getName", [](const air_segment_desc_t &d) -> std::string {
         return {d.name, static_cast<size_t>(d.name_length)};
       });
 
-  pybind11::class_<air_herd_desc_t>(m, "HerdDescriptor")
+  nanobind::class_<air_herd_desc_t>(m, "HerdDescriptor")
       .def("getName", [](const air_herd_desc_t &d) -> std::string {
         return {d.name, static_cast<size_t>(d.name_length)};
       });
@@ -73,9 +74,9 @@ void defineAIRHostModule(pybind11::module &m) {
   m.def("module_unload", &air_module_unload);
 
   m.def("get_module_descriptor", &air_module_get_desc,
-        pybind11::return_value_policy::reference);
+        nanobind::return_value_policy::reference);
 
-  pybind11::class_<hsa_agent_t> Agent(m, "Agent");
+  nanobind::class_<hsa_agent_t> Agent(m, "Agent");
 
   m.def(
       "get_agents",
@@ -84,9 +85,10 @@ void defineAIRHostModule(pybind11::module &m) {
         air_get_agents(agents);
         return agents;
       },
-      pybind11::return_value_policy::reference);
+      nanobind::return_value_policy::reference);
+  // pybind11::return_value_policy::reference);
 
-  pybind11::class_<hsa_queue_t> Queue(m, "Queue");
+  nanobind::class_<hsa_queue_t> Queue(m, "Queue");
 
   m.def(
       "queue_create",
@@ -109,11 +111,11 @@ void defineAIRHostModule(pybind11::module &m) {
           return nullptr;
         return q;
       },
-      pybind11::return_value_policy::reference);
+      nanobind::return_value_policy::reference);
 
   m.def(
       "read32", [](uint64_t addr) -> uint32_t { return air_read32(addr); },
-      pybind11::return_value_policy::copy);
+      nanobind::return_value_policy::copy);
 
   m.def("write32", [](uint64_t addr, uint32_t val) -> void {
     return air_write32(addr, val);
@@ -124,12 +126,12 @@ void defineAIRHostModule(pybind11::module &m) {
       [](uint32_t col, uint32_t row) -> uint64_t {
         return air_get_tile_addr(col, row);
       },
-      pybind11::return_value_policy::copy);
+      nanobind::return_value_policy::copy);
 }
 
 } // namespace
 
-PYBIND11_MODULE(_airRt, m) {
+NB_MODULE(_airRt, m) {
   m.doc() = R"pbdoc(
         AIR Runtime Python bindings
         --------------------------
