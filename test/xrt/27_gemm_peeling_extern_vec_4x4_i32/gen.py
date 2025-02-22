3583f6864bae0a8637fe663d95962d680ccc36ba
erwei-xilinx <erwei.wang@amd.com>
2025-02-21 16:12:56 -0800

    Fixup test 27 by overriding the runtime loop tiling sizes

--- test/xrt/27_gemm_peeling_extern_vec_4x4_i32/gen.py
+++ test/xrt/27_gemm_peeling_extern_vec_4x4_i32/gen.py
@@ -162,5 +162,6 @@ with air.ir.Context() as ctx, Location.unknown():
     backend = XRTBackend(
         lower_linalg_to_func=True,
         air_loop_fusion=True,
+        runtime_loop_tiling_sizes=[2, 2],
     )
     module_function = backend.compile_and_load(air_module)
