6256425f647a557e25b4ebe8ebbdb9a940752f0a
erwei-xilinx <erwei.wang@amd.com>
2025-02-21 18:15:03 -0800

    Fixup runtime_loop_tiling_sizes

--- test/xrt/25_batch_matmul_bf16/gen.py
+++ test/xrt/25_batch_matmul_bf16/gen.py
@@ -162,6 +162,6 @@ with air.ir.Context() as ctx, Location.unknown():
     backend = XRTBackend(
         lower_linalg_to_func=True,
         air_loop_fusion=True,
-        runtime_loop_tiling_sizes=[2, 2],
+        runtime_loop_tiling_sizes=[1, 1],
     )
     module_function = backend.compile_and_load(air_module)
