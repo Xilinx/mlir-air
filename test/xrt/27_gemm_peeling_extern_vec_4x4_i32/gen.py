887fc71996b77b73ab822afbad34bc71e39143d9
erwei-xilinx <erwei.wang@amd.com>
2025-02-25 16:46:39 -0800

    Set air-loop-fusion option to off for some tests as pack-peel tiling strategy doesn't need it

--- test/xrt/27_gemm_peeling_extern_vec_4x4_i32/gen.py
+++ test/xrt/27_gemm_peeling_extern_vec_4x4_i32/gen.py
@@ -160,7 +160,6 @@ with air.ir.Context() as ctx, Location.unknown():
     ###############################################
 
     backend = XRTBackend(
-        air_loop_fusion=True,
         runtime_loop_tiling_sizes=[2, 2],
     )
     module_function = backend.compile_and_load(air_module)
