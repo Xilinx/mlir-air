7cff8a4a9110a17cd0a76471d1afb303e4773e0f
erwei-xilinx <erwei.wang@amd.com>
2025-02-27 21:10:03 -0800

    Purge EnforceLoopCarriedDealloc, as the pass is now part of the canonicalizer

--- mlir/include/air/Transform/AIRDependencyScheduleOpt.h
+++ mlir/include/air/Transform/AIRDependencyScheduleOpt.h
@@ -47,8 +47,6 @@ std::unique_ptr<mlir::Pass> createAIRDependencyScheduleOptPass();
 
 std::unique_ptr<mlir::Pass> createAIRUnrollChannelByFactorPattern();
 
-std::unique_ptr<mlir::Pass> createAIREnforceLoopCarriedMemrefDeallocPattern();
-
 std::unique_ptr<mlir::Pass> createAIRFuseChannels();
 std::unique_ptr<OperationPass<ModuleOp>>
 createAIRFuseChannels(const AIRFuseChannelsOptions &);
