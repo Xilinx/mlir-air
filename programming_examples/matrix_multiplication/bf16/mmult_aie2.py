097f19f2fcc8337cb05a2594f2f4e2fff13259c0
erwei-xilinx <erweiw@xilinx.com>
2025-10-24 18:02:05 -0700

    Support for varying tile sizes for matrix multiplication with runner and profiler support (#1169)

---
+++ programming_examples/matrix_multiplication/bf16/mmult_aie2.py
@@ -0,0 +1,162 @@
+# mmult_aie2.py -*- Python -*-
+#
+# Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+import air.compiler.util
+
+from air.dialects import func, linalg, tensor, arith, memref
+from air.dialects.air import module_builder
+from air.dialects.linalg.opdsl.lang import *
+from air.ir import *
+from air.compiler.util import run_transform
+import air.passmanager
+
+import sys
+import argparse
+import re
+
+# Default values.
+HERD_M = 4
+HERD_N = 4
+
+
+def mmult_runner(air_ir_string: str, herd_m: int = HERD_M, herd_n: int = HERD_N):
+    context = air.ir.Context()
+    air_module = Module.parse(air_ir_string, context=context)
+
+    # generate dependency information for runner
+    pipeline = (
+        "builtin.module("
+        + ",".join(
+            [
+                "air-dependency",
+                "air-hoist-dma-in-accum-pattern",
+                "air-broadcast-detection",
+                "air-specialize-dma-broadcast",
+                "air-dma-to-channel",
+                "canonicalize",
+                "cse",
+                "air-dependency-canonicalize",
+                "canonicalize",
+                "cse",
+                "air-isolate-async-dma-loop-nests",
+                "canonicalize",
+                "cse",
+                "air-fuse-channels",
+                "func.func(air-fuse-alloc-dealloc)",
+                "func.func(air-shrink-memref-sizes-by-access)",
+                "air-label-scf-for-to-ping-pong",
+                "air-ping-pong-transform",
+                "canonicalize",
+                "cse",
+                "air-place-herds{num-rows="
+                + str(herd_m)
+                + " num-cols="
+                + str(herd_n)
+                + " row-anchor=0 col-anchor=0}",
+            ]
+        )
+        + ")"
+    )
+    pm = air.passmanager.PassManager.parse(pipeline, context=context)
+    pm.run(air_module.operation)
+
+    with open("air_ir_debug.mlir", "w") as f:
+        f.write(str(air_module))
+
+    arch = {
+        "clock": 1000000000,
+        "cores": 1,
+        "datatypes": [
+            {"bytes": 1, "name": "i8"},
+            {"bytes": 2, "name": "bf16"},
+            {"bytes": 4, "name": "i32"},
+        ],
+        "devicename": "testdevice",
+        "kernels": {
+            "linalg.copy": {
+                "datatypes": {
+                    "i8": {"ops_per_core_per_cycle": 32, "efficiency": 1},
+                    "bf16": {"ops_per_core_per_cycle": 32, "efficiency": 1},
+                    "i32": {"ops_per_core_per_cycle": 16, "efficiency": 1},
+                },
+                "name": "linalg.copy",
+            },
+            "linalg.fill": {
+                "datatypes": {
+                    "i8": {"ops_per_core_per_cycle": 32, "efficiency": 1},
+                    "bf16": {"ops_per_core_per_cycle": 32, "efficiency": 1},
+                    "i32": {"ops_per_core_per_cycle": 16, "efficiency": 1},
+                },
+                "name": "linalg.fill",
+            },
+            "linalg.generic": {
+                "datatypes": {
+                    "i8": {"macs_per_core_per_cycle": 256, "efficiency": 1},
+                    "bf16": {"macs_per_core_per_cycle": 128, "efficiency": 1},
+                    "i32": {"macs_per_core_per_cycle": 1, "efficiency": 1},
+                },
+                "name": "linalg.generic",
+            },
+            "linalg.matmul": {
+                "datatypes": {
+                    "i8": {"macs_per_core_per_cycle": 256, "efficiency": 1},
+                    "bf16": {"macs_per_core_per_cycle": 128, "efficiency": 1},
+                    "i32": {"macs_per_core_per_cycle": 1, "efficiency": 1},
+                },
+                "name": "linalg.matmul",
+            },
+        },
+        "dus": {
+            "count": [4, 4],
+            "memory": {"memory_space": "L2", "bytes": 524288},
+            "ports": {
+                "outbound": {"count": 6, "bytes_per_second": 4000000000},
+                "inbound": {"count": 6, "bytes_per_second": 4000000000},
+            },
+            "tiles": {
+                "count": [1, 4],
+                "memory": {"memory_space": "L1", "bytes": 65536},
+                "ports": {
+                    "outbound": {"count": 2, "bytes_per_second": 4000000000},
+                    "inbound": {"count": 2, "bytes_per_second": 4000000000},
+                },
+            },
+        },
+        "noc": {
+            "outbound": {"count": 8, "bytes_per_second": 4000000000},
+            "inbound": {"count": 8, "bytes_per_second": 4000000000},
+        },
+    }
+
+    runner = air.compiler.util.Runner(arch, "simulation_trace.json", "core")
+    trace = runner.run(air_module, "matmul_bf16")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="mmult_aie2.py")
+    parser.add_argument(
+        "--input-file",
+        default="input.mlir",
+        type=str,
+        help="Input file containing input IR in AIR dialect",
+    )
+    parser.add_argument(
+        "--herd-m",
+        type=int,
+        default=HERD_M,
+        help="Number of L1 tiles along the M dimension",
+    )
+    parser.add_argument(
+        "--herd-n",
+        type=int,
+        default=HERD_N,
+        help="Number of L1 tiles along the N dimension",
+    )
+    opts = parser.parse_args()
+
+    with open(opts.input_file, "r") as f:
+        air_ir_string = f.read()
+
+    latency = mmult_runner(air_ir_string=air_ir_string)
---
+++ programming_examples/matrix_multiplication/i8/mm_aie2p.cc
@@ -0,0 +1,198 @@
+//===- mm.cc ----------------------------------------------------*- C++ -*-===//
+//
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2025, Advanced Micro Devices, Inc.
+//
+//===----------------------------------------------------------------------===//
+
+#define NOCPP
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <type_traits>
+
+#define REL_WRITE 0
+#define REL_READ 1
+
+#include <aie_api/aie.hpp>
+
+#include "zero.cc"
+
+#define i8_i16_ONLY
+#define DIM_M 64
+#define DIM_K 64
+#define DIM_N 128
+
+template <typename T_in, typename T_out, int rowA, int colA, int colB>
+static inline void matmul_scalar(T_in *a, T_in *b, T_out *c) {
+  event0();
+  for (int row = 0; row < rowA; row++) {
+    for (int col = 0; col < colB; col++) {
+      T_out running_sum = 0;
+      for (int i = 0; i < colA; i++) {
+        running_sum += a[row * colA + i] * b[i * colB + col];
+      }
+      c[row * colB + col] += running_sum;
+    }
+  }
+  event1();
+}
+
+template <typename T_in, typename T_out, unsigned rowA, unsigned colA,
+          unsigned colB, unsigned r, unsigned s, unsigned t>
+void matmul_vectorized(const T_in *__restrict pA, const T_in *__restrict pB,
+                       T_out *__restrict pC) {
+  using MMUL = aie::mmul<r, s, t, T_in, T_in, accauto>;
+
+  event0();
+
+  for (unsigned z = 0; z < rowA; z += 2)
+    chess_loop_range(2, ) {
+      T_out *__restrict pC1 = pC + (z)*MMUL::size_C;
+      T_out *__restrict pC2 = pC + ((z + 1)) * MMUL::size_C;
+
+      for (unsigned j = 0; j < colB; j += 2)
+        chess_prepare_for_pipelining chess_loop_range(8, ) {
+          const T_in *__restrict pA1 = pA + (z)*MMUL::size_A;
+          const T_in *__restrict pA2 = pA + ((z + 1)) * MMUL::size_A;
+          const T_in *__restrict pB1 = pB + (j)*colA * MMUL::size_B;
+          const T_in *__restrict pB2 = pB + ((j + 1)) * colA * MMUL::size_B;
+          aie::vector<T_in, MMUL::size_A> A0 = aie::load_v<MMUL::size_A>(pA1);
+          pA1 += rowA * MMUL::size_A;
+          aie::vector<T_in, MMUL::size_A> A1 = aie::load_v<MMUL::size_A>(pA2);
+          pA2 += rowA * MMUL::size_A;
+          aie::vector<T_in, MMUL::size_B> B0 = aie::load_v<MMUL::size_B>(pB1);
+          pB1 += MMUL::size_B;
+          aie::vector<T_in, MMUL::size_B> B1 = aie::load_v<MMUL::size_B>(pB2);
+          pB2 += MMUL::size_B;
+
+          aie::vector<T_out, MMUL::size_C> acc_C00 =
+              aie::load_v<MMUL::size_C>(pC1);
+          aie::vector<T_out, MMUL::size_C> acc_C01 =
+              aie::load_v<MMUL::size_C>(pC1 + MMUL::size_C * rowA);
+          aie::vector<T_out, MMUL::size_C> acc_C10 =
+              aie::load_v<MMUL::size_C>(pC2);
+          aie::vector<T_out, MMUL::size_C> acc_C11 =
+              aie::load_v<MMUL::size_C>(pC2 + MMUL::size_C * rowA);
+
+          MMUL C00(acc_C00);
+          MMUL C01(acc_C01);
+          MMUL C10(acc_C10);
+          MMUL C11(acc_C11);
+
+          C00.mac(A0, B0);
+          C01.mac(A0, B1);
+          C10.mac(A1, B0);
+          C11.mac(A1, B1);
+
+          for (unsigned i = 1; i < colA; ++i)
+            chess_prepare_for_pipelining chess_loop_range(7, ) {
+              A0 = aie::load_v<MMUL::size_A>(pA1);
+              pA1 += rowA * MMUL::size_A;
+              A1 = aie::load_v<MMUL::size_A>(pA2);
+              pA2 += rowA * MMUL::size_A;
+              B0 = aie::load_v<MMUL::size_B>(pB1);
+              pB1 += MMUL::size_B;
+              B1 = aie::load_v<MMUL::size_B>(pB2);
+              pB2 += MMUL::size_B;
+              C00.mac(A0, B0);
+              C01.mac(A0, B1);
+              C10.mac(A1, B0);
+              C11.mac(A1, B1);
+            }
+
+          aie::store_v(pC1, C00.template to_vector<T_out>());
+          pC1 += MMUL::size_C * rowA;
+          aie::store_v(pC1, C01.template to_vector<T_out>());
+          pC1 += MMUL::size_C * rowA;
+          aie::store_v(pC2, C10.template to_vector<T_out>());
+          pC2 += MMUL::size_C * rowA;
+          aie::store_v(pC2, C11.template to_vector<T_out>());
+          pC2 += MMUL::size_C * rowA;
+        }
+    }
+
+  event1();
+}
+
+// int8 MatMul kernel definion with int16 outputs.
+template <unsigned m, unsigned k, unsigned n>
+static inline void matmul_vectorized_8x8x8_i8_i16(const int8 *__restrict pA,
+                                                  const int8 *__restrict pB,
+                                                  int16 *__restrict pC) {
+
+  // After extensive experimentation, the 4x8x8 aie::mmul size was found to be
+  // optimal for AIE2, in combination with the 4x2 mmul expanded kernel
+  constexpr int r = 8;
+  constexpr int s = 8;
+  constexpr int t = 8;
+
+  // Since the kernel has been expanded 4 times for A ('m' dimension) and 2
+  // times for B ('n' dimension), the following assertions veirify this even
+  // division for the single AIE MatMul dimensionality Notice that 'k' dimension
+  // is not spatially expanded.
+  static_assert(m % (4 * r) == 0); // 'm' dimension
+  static_assert(k % s == 0);       // 'k' dimension
+  static_assert(n % (2 * t) == 0); // 'n' dimension
+
+  return matmul_vectorized<int8, int16, (m / r), (k / s), (n / t), r, s, t>(
+      pA, pB, pC);
+}
+
+extern "C" {
+
+// If you want to compile microkernels with different inner tile sizes,
+// define DIM_M, DIM_K and DIM_N at compile time using -DDIM_M 32 etc.
+// These dimensions must be divisible by the r, s, t dimensions used in
+// the kernels.
+
+#ifndef DIM_M
+#define DIM_M 64
+#endif
+
+#ifndef DIM_K
+#define DIM_K 64
+#endif
+
+#ifndef DIM_N
+#define DIM_N 64
+#endif
+
+#ifndef combos
+#define combos(X) X(int8, i8, int16, i16, 8, 8, 8)
+#endif
+
+#define matmul_vectorized_c_func(ctype_in, mlir_type_in, ctype_out,            \
+                                 mlir_type_out, r, s, t)                       \
+  void op_has_no_registered_library_name(ctype_in *a_in, ctype_in *b_in,       \
+                                         ctype_out *c_out) {                   \
+    matmul_vectorized_##r##x##s##x##t##_##mlir_type_in##_##mlir_type_out<      \
+        DIM_M, DIM_K, DIM_N>(a_in, b_in, c_out);                               \
+  }
+
+#define matmul_scalar_c_func(ctype_in, mlir_type_in, ctype_out, mlir_type_out, \
+                             r, s, t)                                          \
+  void matmul_scalar_##mlir_type_in##_##mlir_type_out(                         \
+      ctype_in *a_in, ctype_in *b_in, ctype_out *c_out) {                      \
+    matmul_scalar<ctype_in, ctype_out, DIM_M, DIM_K, DIM_N>(a_in, b_in,        \
+                                                            c_out);            \
+  }
+
+#define zero_scalar_c_func(ctype_in, mlir_type_in, ctype_out, mlir_type_out,   \
+                           r, s, t)                                            \
+  void zero_scalar_##mlir_type_out(ctype_out *c_out) {                         \
+    zero_scalar<ctype_out, DIM_M, DIM_N>(c_out);                               \
+  }
+
+#define zero_vectorized_c_func(ctype_in, mlir_type_in, ctype_out,              \
+                               mlir_type_out, r, s, t)                         \
+  void linalg_fill_##mlir_type_out##_view1x1x16x8x8x8x##mlir_type_out##as2(    \
+      ctype_out *c_out) {                                                      \
+    zero_vectorized<ctype_out, DIM_M, DIM_N>(c_out);                           \
+  }
+
+combos(matmul_vectorized_c_func) combos(matmul_scalar_c_func)
+    combos(zero_vectorized_c_func) combos(zero_scalar_c_func)
+
+} // extern "C"
