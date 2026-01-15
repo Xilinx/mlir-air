0fc496a0823608379f68a47a136fec6af611376a
amd-jiajli <jiajli@amd.com>
2026-01-15 00:10:32 -0500

    Update env_setup.sh to include PEANO_INSTALL_DIR
    - Modified utils/env_setup.sh to accept 4 arguments instead of 3
    - Added PEANO_INSTALL_DIR (llvm-aie) as the 3rd argument
    - Updated docs/buildingRyzenLin.md with new env_setup.sh usage
    - PEANO_INSTALL_DIR is now automatically set by env_setup.sh script
    
    This change centralizes PEANO compiler configuration and improves
    consistency across different installation methods.

--- utils/env_setup.sh
+++ utils/env_setup.sh
@@ -7,18 +7,19 @@
 # This script sets up the environment to run the mlir-aie build tools.
 # It must be sourced, not executed.
 #
-# e.g. source env_setup.sh /scratch/mlir-air/install /scratch/mlir-aie/install /scratch/llvm/install
+# e.g. source env_setup.sh /scratch/mlir-air/install /scratch/mlir-aie/install /scratch/llvm-aie/install /scratch/llvm/install
 #
 ##===----------------------------------------------------------------------===##
 
-if [ "$#" -ne 3 ]; then
-	echo "ERROR: Needs 3 arguments for <mlir-air install dir> <mlir-aie install dir> and <llvm install dir>"
+if [ "$#" -ne 4 ]; then
+	echo "ERROR: Needs 4 arguments for <mlir-air install dir> <mlir-aie install dir> <llvm-aie install dir> and <llvm install dir>"
 	return
 fi
 
 export MLIR_AIR_INSTALL_DIR=`realpath $1`
 export MLIR_AIE_INSTALL_DIR=`realpath $2`
-export LLVM_INSTALL_DIR=`realpath $3`
+export PEANO_INSTALL_DIR=`realpath $3`
+export LLVM_INSTALL_DIR=`realpath $4`
 
 export PATH=${MLIR_AIR_INSTALL_DIR}/bin:${MLIR_AIE_INSTALL_DIR}/bin:${LLVM_INSTALL_DIR}/bin:${PATH} 
 export PYTHONPATH=${MLIR_AIR_INSTALL_DIR}/python:${MLIR_AIE_INSTALL_DIR}/python:${PYTHONPATH} 
