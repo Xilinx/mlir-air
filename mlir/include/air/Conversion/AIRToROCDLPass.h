f4b1e8c3493ac1b7e74248051da9c952af78fc4b
Sangeeta Chowdhary <sangeeta.chowdhary@amd.com>
2026-01-27 18:59:15 +0000

    fixed formatting issues

--- mlir/include/air/Conversion/AIRToROCDLPass.h
+++ mlir/include/air/Conversion/AIRToROCDLPass.h
@@ -1,6 +1,33 @@
-//===- AIRToROCDLPass.h - Convert AIR to ROCDL Pass -----------------*- C++ -*-===//
+//===- AIRToROCDLPass.h -------------------------------------------*- C++ -*-===//
 //
-// This file declares the pass that converts AIR dialect to ROCDL dialect.
+// Copyright (C) 2021-2022, Xilinx Inc. All rights reserved.
+// Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CONVERT_TO_AIR_ROCDL
+#define CONVERT_TO_AIR_ROCDL
+
+#include "air/Conversion/PassDetail.h"
+
+#include "mlir/Pass/Pass.h"
+#include <memory>
+
+namespace xilinx {
+namespace air {
+
+std::unique_ptr<mlir::Pass> createAIRToROCDLPass();
+
+} // namespace air
+} // namespace xilinx
+#endif // CONVERT_TO_AIR_ROCDL
+//===- AIRToROCDLPass.h -------------------------------------------*- C++
+//-*-===//
+//
+// Copyright (C) 2021-2022, Xilinx Inc. All rights reserved.
+// Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+// SPDX-License-Identifier: MIT
 //
 //===----------------------------------------------------------------------===//
 
--- mlir/include/air/Conversion/GPUKernelOutlinePass.h
+++ mlir/include/air/Conversion/GPUKernelOutlinePass.h
@@ -1,6 +1,33 @@
-//===- GPUKernelOutlinePass.h - Convert AIR to ROCDL Pass -----------------*- C++ -*-===//
+//===- GPUKernelOutlinePass.h -------------------------------------------*- C++ -*-===//
 //
-// This file declares the pass that converts AIR dialect to ROCDL dialect.
+// Copyright (C) 2021-2022, Xilinx Inc. All rights reserved.
+// Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CONVERT_TO_GPU_OUTLINE
+#define CONVERT_TO_GPU_OUTLINE
+
+#include "air/Conversion/PassDetail.h"
+
+#include "mlir/Pass/Pass.h"
+#include <memory>
+
+namespace xilinx {
+namespace air {
+
+std::unique_ptr<mlir::Pass> createGPUKernelOutlinePass();
+
+} // namespace air
+} // namespace xilinx
+#endif // CONVERT_TO_GPU_OUTLINE
+//===- GPUKernelOutlinePass.h -------------------------------------------*- C++
+//-*-===//
+//
+// Copyright (C) 2021-2022, Xilinx Inc. All rights reserved.
+// Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+// SPDX-License-Identifier: MIT
 //
 //===----------------------------------------------------------------------===//
 
